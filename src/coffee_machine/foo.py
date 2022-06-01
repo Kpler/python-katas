78b1c9f40eb91d2c17e791b2304e3a5b0807bc3e
szeswee <song@kpler.com>
2022-06-01 17:32:09 +0800

    szeswee

--- src/coffee_machine/foo.py
+++ src/coffee_machine/foo.py
@@ -1,2 +1,10 @@
 def is_valid_order(order: str) -> bool:
-    return True
+    parts = order.split(":")
+
+    if len(parts) != 3:
+        return False
+
+    if parts[0] in {"T", "H", "C"}:
+        return True
+
+    return False
--- tests/coffee_machine/foo.py
+++ tests/coffee_machine/foo.py
@@ -2,3 +2,19 @@ from src.coffee_machine.foo import is_valid_order
 
 def test_1():
     assert is_valid_order("") is False
+
+
+def test_2():
+    assert is_valid_order("::") is False
+
+
+def test_order_tea():
+    assert is_valid_order("T::") is True
+
+
+def test_order_chocolate():
+    assert is_valid_order("H::") is True
+
+
+def test_order_coffee():
+    assert is_valid_order("C::") is True
