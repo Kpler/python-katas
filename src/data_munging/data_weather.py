04d79f95f2eaf6fe667cefcd6feb7e4b23a24ac4
antoine charlet <acharlet@kpler.com>
2022-06-15 11:45:59 +0200

    antoine

--- src/data_munging/data_weather.py
+++ src/data_munging/data_weather.py
@@ -1,6 +1,5 @@
 import csv
 
-
 def retrieve_csv(path: str):
     with open(path) as file:
         reader = csv.reader(file, delimiter=",")
@@ -9,5 +8,7 @@ def retrieve_csv(path: str):
         return list(reader)
 
 
-def find_minimum_spread_date(weather_data: list):
-    return 2
+def find_minimum_spread_date(weather_data: list) -> int:
+    weather_data = weather_data[:-1]
+    return [(r[0],float(r[1].replace("*","")) - float(r[2].replace("*",""))) 
+            for r in weather_data][0][0]
