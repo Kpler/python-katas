0ae81851be825406d24c263aba644f3f7c3337b7
Léo <ldureuil@kpler.com>
2022-06-15 11:30:20 +0200

    leo

--- tests/data_munging/test_data_weather.py
+++ tests/data_munging/test_data_weather.py
@@ -2,12 +2,12 @@ import pytest
 from src.data_munging.data_weather import retrieve_csv
 
 
-# PATH = "./tests/data_munging/weather.csv"
 PATH = (
-    "/Users/pietroguardati/PycharmProjects/python-katas/tests/data_munging/football.csv"
+    "tests/data_munging/weather.csv"
 )
 
 
 def test_read_csv():
     data = retrieve_csv(PATH)
-    assert data is not None
+    print(data)
+    assert len(data) == 31
