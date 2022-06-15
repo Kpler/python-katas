fc24aaab8c1ac40df801c31676245b9a29638c68
Léo <ldureuil@kpler.com>
2022-06-15 11:59:42 +0200

    leo

--- src/data_munging/data_weather.py
+++ src/data_munging/data_weather.py
@@ -6,13 +6,18 @@ def retrieve_csv(path: str):
         reader = csv.reader(file, delimiter=",")
         _ = next(reader)
 
-        return list(reader)
+        lines = []
+        for r in list(reader)[:-1]:
+            lines.append([int(r[0]),
+                          float(r[1].replace("*", "")),
+                          float(r[2].replace("*", ""))])
+        return lines
 
 
 def find_minimum_spread_date(weather_data: list) -> int:
     weather_data = weather_data[:-1]
     date_and_spread = [
-        (int(r[0]), float(r[1].replace("*", "")) - float(r[2].replace("*", "")))
+        r[0], r[1] - r[2]
         for r in weather_data
     ]
     return min(date_and_spread, key=lambda x: x[1])[0]
