6c99af9606ab8a61bbea198fca6fe7eb5c2239e5
Namsam <snaim@kpler.com>
2022-06-23 15:56:26 +0200

    samir

--- src/data_munging/data_football.py
+++ src/data_munging/data_football.py
@@ -1,4 +1,13 @@
 import csv
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Row:
+    name: str
+    goals_scored: int
+    goals_taken: int
+
 
 def retrieve_csv(path: str):
     with open(path) as file:
@@ -8,9 +17,9 @@ def retrieve_csv(path: str):
         lines = []
         for r in list(reader)[:-1]:  # '-1' skip average row
             row = Row(
-                date=int(r[0]),
-                max_temperature=float(r[1].replace("*", "")),
-                min_temperature=float(r[2].replace("*", "")),
+                name=r[1],
+                goals_scored=int(r[6]),
+                goals_taken=int(r[7]),
             )
             lines.append(row)
         return lines
