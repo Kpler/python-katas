69aca355507811ef748b08b4236c9195aebb0661
Germain Geoffroy <ggeoffroy@kpler.com>
2022-06-28 10:36:28 +0200

    samir papa

--- src/data_munging/data_common.py
+++ src/data_munging/data_common.py
@@ -16,13 +16,8 @@ def retrieve_csv(path: str):
     with open(path) as file:
         reader = csv.reader(file, delimiter=",")
         _ = next(reader)  # skip csv headers
-
+        
         lines = []
         for r in list(reader):  # '-1' skip average row
-            row = Row(
-                label=str(r[0]),
-                max_value=float(r[1].replace("*", "")),
-                min_value=float(r[2].replace("*", "")),
-            )
-            lines.append(row)
+            lines.append(r)
         return lines
--- tests/data_munging/test_data_common.py
+++ tests/data_munging/test_data_common.py
@@ -1,6 +1,12 @@
 from src.data_munging.data_common import retrieve_csv
 
+PATH = "tests/data_munging/football.csv"
 
 def test_retrieve_csv():
-    lines = retrieve_csv(path)
+    lines = retrieve_csv(path=PATH)
     assert len(lines) == 20
+
+def test_row_delta():
+    row = "1,88,59,74,,53.8,,0.00,F,280,9.6,270,17,1.6,93,23,1004.5"
+    res = parse_row(row)
+    assert res.delta == 29
