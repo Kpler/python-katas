6f0d0c864777231ace8529303e287a4c74f885fd
Namsam <snaim@kpler.com>
2022-06-28 10:43:20 +0200

    samir

--- src/data_munging/data_common.py
+++ src/data_munging/data_common.py
@@ -12,6 +12,7 @@ class Row:
     def delta(self) -> float:
         return self.max_value - self.min_value
 
+
 def retrieve_csv(path: str):
     with open(path) as file:
         reader = csv.reader(file, delimiter=",")
@@ -21,3 +22,15 @@ def retrieve_csv(path: str):
         for r in list(reader):  # '-1' skip average row
             lines.append(r)
         return lines
+
+
+def parse_row(raw: list, index_label: int, index_min: int, index_max: int):
+    return Row(
+        label=raw[index_label],
+        min_value=float(raw[index_min]),
+        max_value=float(raw[index_max])
+    )
+
+
+def get_min_spread(data: list[Row]):
+    return min(data, key=lambda row: abs(row.spread)).label
--- tests/data_munging/test_data_common.py
+++ tests/data_munging/test_data_common.py
@@ -1,12 +1,19 @@
-from src.data_munging.data_common import retrieve_csv
+from src.data_munging.data_common import get_min_spread, parse_row, retrieve_csv
 
 PATH = "tests/data_munging/football.csv"
+
 
 def test_retrieve_csv():
     lines = retrieve_csv(path=PATH)
     assert len(lines) == 20
 
+
 def test_row_delta():
-    row = "1,88,59,74,,53.8,,0.00,F,280,9.6,270,17,1.6,93,23,1004.5"
-    res = parse_row(row)
+    row = "1,88,59,74,,53.8,,0.00,F,280,9.6,270,17,1.6,93,23,1004.5".split(",")
+    res = parse_row(row, 0, 2, 1)
     assert res.delta == 29
+
+
+def test_get_min_spread():
+    data = retrieve_csv(PATH)
+    assert get_min_spread(data) == "Aston_Villa"
