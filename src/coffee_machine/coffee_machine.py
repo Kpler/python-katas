6ae2b3878a4bc43bb659395a1f2e6a5cede29058
Henry Nomichith <hnomichith@kpler.com>
2022-07-12 17:39:43 +0200

    henry

---
+++ src/coffee_machine/coffee_machine.py
@@ -0,0 +1,7 @@
+
+
+def make_drink(input: str) -> str:
+    if input=="tea":
+        return "T:1:0"
+    elif input=="chocolate":
+        return "H::"
---
+++ tests/coffee_machine/test_coffee_machine.py
@@ -0,0 +1,16 @@
+from src.coffee_machine.coffee_machine import make_drink
+
+
+def test_make_tea_with_one_sugar():
+    result = make_drink("tea")
+    assert result == "T:1:0"
+
+
+def test_make_chocolate_without_sugar():
+    result = make_drink("chocolate")
+    assert result == "H::"
+
+
+def test_make_tea_without_sugar():
+    result = make_drink("tea")
+    assert result == "T::"
