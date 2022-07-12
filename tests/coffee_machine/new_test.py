d98376426dd66cd1a86cff5696d019de667c7cf1
fabienbousquet <fbousquet@kpler.com>
2022-07-12 17:32:40 +0200

    class creation

--- tests/coffee_machine/new_test.py
+++ tests/coffee_machine/new_test.py
@@ -1,6 +1,13 @@
 import pytest
 def make_new_order():
     pass
+from dataclasses import dataclasses
+class Drinkorder : 
+    drink_type = str
+    
+
+
 
 def test_new_order():
     assert make_new_order() == "T:1:0"
+
