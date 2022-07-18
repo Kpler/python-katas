7c5c717de692f62c22f3ecc4d9c920bbb5c3b47c
mlyukpler <mlyu@kpler.com>
2022-07-17 22:24:59 -0400

    test coffee maker

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -1,14 +1,28 @@
 
 
-def make_drink(input: str, number_of_sugar: int) -> str:
+def make_drink(input: str, number_of_sugar: str, number_of_stick: str) -> str:
     first_char: str
     
     if input=="tea":
       first_char="T"
     elif input=="chocolate":
       first_char="H"
+    elif input=="coffee":
+      first_char="C"
 
-    second_char: int
-    if
+    second_char: str
 
-    third_char: int
+    if number_of_sugar == "1":
+      second_char= "1"
+    elif number_of_sugar == "0":
+      second_char = "0"
+    elif number_of_sugar == "2":
+      second_char = "2"
+
+    third_char: str
+
+    if number_of_stick == "1":
+      third_char= "1"
+    elif number_of_stick == "0":
+      third_char = "0"
+    return first_char + ":" + second_char + ":" + third_char
--- tests/coffee_machine/test_coffee_machine.py
+++ tests/coffee_machine/test_coffee_machine.py
@@ -2,15 +2,15 @@ from src.coffee_machine.coffee_machine import make_drink
 
 
 def test_make_tea_with_one_sugar():
-    result = make_drink("tea", 1)
-    assert result == "T:1:0"
+    result = make_drink("tea", "1", "1")
+    assert result == "T:1:1"
 
 
 def test_make_chocolate_without_sugar():
-    result = make_drink("chocolate", 0)
-    assert result == "H::"
+    result = make_drink("chocolate", "0", "0")
+    assert result == "H:0:0"
 
 
-def test_make_tea_without_sugar():
-    result = make_drink("tea")
-    assert result == "T::"
+def test_make_coffee_without_sugar():
+    result = make_drink("coffee", "2", "1")
+    assert result == "C:2:1"
