4723e7b0c34f53085377e6e9f38f57846d7c20c8
Nicolas Guignard <nicolas065@gmail.com>
2022-07-26 17:46:56 +0200

    WIP

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -1,6 +1,9 @@
-def order(order_type: str, nb_of_sugars: int = 0):
+def order(order_type: str, nb_of_sugars: int = 0) -> str:
     if order_type == 'H':
         return 'H::'
-    else:
+    elif order_type == 'T':
         return 'T::'
-
+    elif order_type == 'C':
+        return 'C::'
+    else:
+        return 'M:Unsupported Drink'
--- tests/coffee_machine/coffee_machine.py
+++ tests/coffee_machine/coffee_machine.py
@@ -3,9 +3,20 @@ from src.coffee_machine.coffee_machine import order
 
 def test_should_order_simple_hot_chocolate():
     result = order('H')
-    assert result is 'H::'
+    assert result == 'H::'
 
 def test_should_order_simple_tea():
     result = order('T')
-    assert result is 'T::'
+    assert result == 'T::'
 
+def test_should_order_simple_coffee():
+    result = order('C')
+    assert result == 'C::'
+
+def test_should_not_order_anything_if_not_a_tea_coffee_hot_chocolate():
+    result = order('')
+    assert result == 'M:Unsupported Drink'
+
+# def test_should_order_simple_coffee():
+#     result = order('C')
+#     assert result == 'C::'
