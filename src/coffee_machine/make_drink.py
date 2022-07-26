685bdeb3b25622ffdcd4f412cd352e81827c3b13
Cassie Brooks <cassbrooks@outlook.com>
2022-07-26 17:53:49 +0200

    cassie2

--- src/coffee_machine/make_drink.py
+++ src/coffee_machine/make_drink.py
@@ -1,8 +1,18 @@
+from logging import exception
 from typing import Optional
 
 def make_drink(drink: str, sugar = 0):
+  if sugar < 0:
+    exception.valueError('Sugar may not be negative')
+
+  drink_code: str
   if drink == "coffee":
     drink_code = 'C'
-    if sugar > 0:
-      return 'C:' + str(sugar) + ':1'
-    return 'C::'
+  elif drink == 'tea':
+    drink_code = 'T'
+  elif drink == 'chocolate':
+    drink_code = 'H'
+
+  if sugar > 0:
+    return drink_code + ':' + str(sugar) + ':1'
+  return drink_code + '::'
--- src/coffee_machine/test_make_drink.py
+++ src/coffee_machine/test_make_drink.py
@@ -1,8 +1,16 @@
 from src.coffee_machine.make_drink import make_drink
-
+import pytest
 
 def test_coffee():
   assert make_drink("coffee") == 'C::'
   
 def test_coffee_with_sugar():
   assert make_drink("coffee", 1) == 'C:1:1'
+
+def test_super_sugary_chocolate():
+  assert make_drink("chocolate", 10) == 'H:10:1'
+
+def test_negative_sugar():
+  with pytest.raises(Exception):
+    make_drink("tea", -3) 
+
