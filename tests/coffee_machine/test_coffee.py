aba5f0a3bbcf60c61c29f6f660b58c121a852ad9
akulenovic97 <akulenovic97@gmail.com>
2022-07-26 11:37:35 -0400

    no sugar test (aldin)

--- tests/coffee_machine/test_coffee.py
+++ tests/coffee_machine/test_coffee.py
@@ -27,10 +27,17 @@ class CoffeeTestCase(unittest.TestCase):
         # THEN
         self.assertEqual(result, "T:1:0")  # add assertion here
 
+    def test_no_sugar_order(self):
+        drink_type = DrinkType.CHOCOLATE
+        sugar = 0
+        result = self.make_order(drink_type, sugar)
+        self.assertEqual(result, "H::")
 
-    def make_order(self, drink_type, sugar):
+
+    def make_order(self, drink_type: DrinkType, sugar: int):
+        sugar_str = str(sugar) if sugar > 0 else ''
         stick = '0' if sugar > 0 else ''
-        return ':'.join([drink_type.to_drink_maker_protocol(), str(sugar), stick])
+        return ':'.join([drink_type.to_drink_maker_protocol(), sugar_str, stick])
 
 
 if __name__ == '__main__':
