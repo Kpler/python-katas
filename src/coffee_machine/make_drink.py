d9add50c24f5b6cad9d4b9124f5fb10ba14fcf48
DavidPalmi <dpalmieri@kpler.com>
2022-07-26 11:44:05 -0400

    david

--- src/coffee_machine/make_drink.py
+++ src/coffee_machine/make_drink.py
@@ -2,4 +2,7 @@ from typing import Optional
 
 def make_drink(drink: str, sugar = 0):
   if drink == "coffee":
+    drink_code = 'C'
+    if sugar > 0:
+      return 'C:' + str(sugar) + ':1'
     return 'C::'
--- src/coffee_machine/test_make_drink.py
+++ src/coffee_machine/test_make_drink.py
@@ -3,3 +3,6 @@ from src.coffee_machine.make_drink import make_drink
 
 def test_coffee():
   assert make_drink("coffee") == 'C::'
+  
+def test_coffee_with_sugar():
+  assert make_drink("coffee", 1) == 'C:1:1'
