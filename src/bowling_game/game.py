b81cc53fafdabc3a5115de257a763d0880d9e266
evelynli-12ssh-keyscan <eli@kpler.com>
2022-08-09 11:51:29 -0400

    eve

--- src/bowling_game/game.py
+++ src/bowling_game/game.py
@@ -4,9 +4,16 @@ class Game:
     current_score = 0
 
     def score(self) -> int:
-      print(self.rolls)
-      self.current_score = sum(self.rolls)
+        for i , value in enumerate(self.rolls):
+
+
+
+      if (pins + self.rolls[self.rolls.length - 1]) == 10:
+        self.current_score = sum(self.rolls)
       return self.current_score
 
     def roll(self, pins: int):
+
+
+
       self.rolls.append(pins)
