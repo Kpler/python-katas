777f4d156af60c865e6626597c1e8f348a7cf13d
Nicolas Guignard <nicolas065@gmail.com>
2022-08-09 17:31:49 +0200

    make tests pass + add spare test

--- tests/bowling_game/game.py
+++ tests/bowling_game/game.py
@@ -13,12 +13,20 @@ def test_all_gutters_should_score_zero():
 
 def test_basic_game_should_add_up_pins():
     game = Game()
-
-    print("Start score: " + str(game.current_score))
-
-    for i in range(0, 21):
+    for i in range(1, 21):
         game.roll(1)
-        print(f"Current score: {game.current_score}")
     final_score = game.score()
 
     assert final_score == 20
+
+
+def test_spare_should_add_the_next_roll_as_bonus():
+    game = Game()
+    game.roll(3)
+    game.roll(7) # 10 + next roll (1) --> 11
+
+    for i in range(3, 21):
+        game.roll(1)
+    final_score = game.score()
+
+    assert final_score == 29
