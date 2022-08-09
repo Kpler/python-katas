924fd89153b9e908a60a5f4eaba0dd4f1f566625
akulenovic97 <akulenovic97@gmail.com>
2022-08-09 11:57:42 -0400

    final commit, aldin

--- src/bowling_game/game.py
+++ src/bowling_game/game.py
@@ -3,9 +3,14 @@ class Game:
     rolls = []
     current_score = 0
 
+    # [3, 7, 1, 2]
+    #  0  1   2  3  4
+    # [3, 7 + 1, 1, 2]
+
     def score(self) -> int:
         for i , value in enumerate(self.rolls):
-
+            self.current_score
+            if i % 2 != 0 and sum(self.rolls[i-2], self.rolls[i-1]) == 10:
 
 
       if (pins + self.rolls[self.rolls.length - 1]) == 10:
@@ -13,7 +18,4 @@ class Game:
       return self.current_score
 
     def roll(self, pins: int):
-
-
-
       self.rolls.append(pins)
