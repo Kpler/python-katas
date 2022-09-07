61d7c99680c054ab4f6c2ee043f0519db0a47450
austin-swertfager <62552448+austin-swertfager@users.noreply.github.com>
2022-09-07 13:42:19 -0400

    Committing two working tests.

--- src/bowling_game/game.py
+++ src/bowling_game/game.py
@@ -1,2 +1,4 @@
 class Game:
-    pass
+    score = 0
+    def roll(self,count):
+        self.score += count
--- tests/bowling_game/game.py
+++ tests/bowling_game/game.py
@@ -6,3 +6,9 @@ def test_simple_score():
     game = Game()
     game.roll(4)
     assert(game.score == 4)
+    
+def test_two_rolls():
+    game = Game()
+    game.roll(4)
+    game.roll(6)
+    assert(game.score == 10)
