ce44572a5ce54cdec21f247071cdafc217260efb
Grégoire Chauvet <gchauvet.home@gmail.com>
2022-09-07 21:15:17 +0200

    It works

--- src/bowling_game/game.py
+++ src/bowling_game/game.py
@@ -5,13 +5,27 @@ class Game:
     def roll(self, count):
         self.rolls.append(count)
 
+        if count == 10 and len(self.rolls) % 2 == 1:
+            self.rolls.append(0)
+
+    def is_spare(self, index):
+        is_even = index%2 == 0
+        penultimate_roll = self.rolls[index-2] if index >= 2 else 0
+        previous_roll = self.rolls[index-1] if index >= 2 else 0
+        return penultimate_roll + previous_roll == 10 and penultimate_roll != 10 and is_even
+
+    def is_strike(self, index):
+        is_odd = index%2 == 1
+        antipenultimate_roll = self.rolls[index-3] if index >= 3 else 0
+        penultimate_roll = self.rolls[index-2] if index >= 2 else 0
+        return (antipenultimate_roll == 10 and is_odd) or (penultimate_roll == 10 and not is_odd)
+
     def score(self):
         output = 0
-        for index,roll in enumerate(self.rolls):
-            is_even = index%2 == 0
-            penultimate_roll = self.rolls[index-2] if index >= 2 else 0
-            previous_roll = self.rolls[index-1] if index >= 2 else 0
+        for index, roll in enumerate(self.rolls):
             output += roll
-            if penultimate_roll + previous_roll == 10 and is_even:
+            if self.is_spare(index):
+                output += roll
+            if self.is_strike(index):
                 output += roll
         return output
--- tests/bowling_game/game.py
+++ tests/bowling_game/game.py
@@ -25,3 +25,18 @@ def test_invalid_spare():
     game.roll(5)
     game.roll(3)
     assert(game.score() == 17)
+
+def test_strike():
+    game = Game()
+    game.roll(10)
+    game.roll(5)
+    game.roll(3)
+    assert(game.score() == 26)
+
+def test_strike_like_spare():
+    game = Game()
+    game.roll(0)
+    game.roll(10)
+    game.roll(3)
+    game.roll(2)
+    assert(game.score() == 18)
