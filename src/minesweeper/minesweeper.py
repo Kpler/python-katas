19da7645059bca366922bba2049bd043734e7606
austin-swertfager <62552448+austin-swertfager@users.noreply.github.com>
2022-09-12 17:57:39 -0400

    Working iteration 2.

--- src/minesweeper/minesweeper.py
+++ src/minesweeper/minesweeper.py
@@ -9,10 +9,18 @@ class Minesweeper:
         for i in range(self.rows):
             inner_grid = []
             for j in range(self.columns):
-                inner_grid.append(".")
+                inner_grid.append("0")
             grid.append(inner_grid)
 
         for (row,column) in self.bombs:
             grid[row][column] = "*"
+            for i in range(row-1,row+2):
+                for j in range(column-1,column+2):
+                    if i > self.rows-1 or i < 0 or j > self.columns-1 or j < 0 or grid[i][j] == "*":
+                        continue
+                    temp_int = int(grid[i][j])
+                    temp_int += 1
+                    grid[i][j] = str(temp_int)
+
 
         return "\n".join(["".join(row) for row in grid])
--- tests/minesweeper/test_minesweeper.py
+++ tests/minesweeper/test_minesweeper.py
@@ -3,8 +3,16 @@ from src.minesweeper.minesweeper import Minesweeper
 
 def test_minesweeper_display() -> None:
     minesweeper = Minesweeper(4, 4, [])
-    assert minesweeper.display() == "....\n....\n....\n...."
+    assert minesweeper.display() == "0000\n0000\n0000\n0000"
 
 def test_bomb_insert() -> None:
-    minesweeper = Minesweeper(4, 4, [(1,1)])
-    assert minesweeper.display() == "....\n.*..\n....\n...."
+    minesweeper = Minesweeper(4, 4, [(0,0)])
+    assert minesweeper.display() == "*100\n1100\n0000\n0000" 
+
+def test_bomb_insert_1() -> None:
+    minesweeper = Minesweeper(4, 4, [(3,3)])
+    assert minesweeper.display() == "0000\n0000\n0011\n001*"
+
+def test_bomb_insert_2() -> None:
+    minesweeper = Minesweeper(4, 4, [(3,3),(2,2)])
+    assert minesweeper.display() == "0000\n0111\n01*2\n012*"
