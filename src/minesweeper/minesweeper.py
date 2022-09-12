f8f2a0a5f6b6a8ebedfbe3a281a29c010ed877c7
austin-swertfager <62552448+austin-swertfager@users.noreply.github.com>
2022-09-12 16:10:42 -0400

    print join struggles.

--- src/minesweeper/minesweeper.py
+++ src/minesweeper/minesweeper.py
@@ -1,2 +1,16 @@
 class Minesweeper:
-    pass
+    def __init__(self,rows:int,columns:int,bombs:list[tuple[int,int]]) -> None:
+        self.rows = rows
+        self.columns = columns
+        pass
+
+    def display(self) -> str:
+        grid = []
+        for i in range(self.rows):
+            inner_grid = []
+            for j in range(self.columns):
+                inner_grid.append(".")
+            grid.append(inner_grid)
+        # print (grid[].join(grid))
+        print(grid)
+        return ""
--- tests/minesweeper/test_minesweeper.py
+++ tests/minesweeper/test_minesweeper.py
@@ -1,6 +1,5 @@
 from src.minesweeper.minesweeper import Minesweeper
 
-
 def test_minesweeper_display() -> None:
     minesweeper = Minesweeper(4, 4, [])
     assert minesweeper.display() == "....\n....\n....\n...."
