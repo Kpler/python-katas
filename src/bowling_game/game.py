e6e47a9454c3bc0df06f28b04073da39c0641a0e
Grégoire Chauvet <gchauvet.home@gmail.com>
2022-09-12 20:46:59 +0200

    Add mypy and type annotations

--- src/bowling_game/game.py
+++ src/bowling_game/game.py
@@ -1,26 +1,26 @@
 class Game:
-    def __init__(self):
-        self.rolls = []
+    def __init__(self) -> None:
+        self.rolls: list[int] = []
 
-    def roll(self, count):
+    def roll(self, count: int) -> None:
         self.rolls.append(count)
 
         if count == 10 and len(self.rolls) % 2 == 1:
             self.rolls.append(0)
 
-    def is_spare(self, index):
+    def is_spare(self, index: int) -> bool:
         is_even = index%2 == 0
         penultimate_roll = self.rolls[index-2] if index >= 2 else 0
         previous_roll = self.rolls[index-1] if index >= 2 else 0
         return penultimate_roll + previous_roll == 10 and penultimate_roll != 10 and is_even
 
-    def is_strike(self, index):
+    def is_strike(self, index: int) -> bool:
         is_odd = index%2 == 1
         antipenultimate_roll = self.rolls[index-3] if index >= 3 else 0
         penultimate_roll = self.rolls[index-2] if index >= 2 else 0
         return (antipenultimate_roll == 10 and is_odd) or (penultimate_roll == 10 and not is_odd)
 
-    def score(self):
+    def score(self) -> int:
         output = 0
         for index, roll in enumerate(self.rolls):
             output += roll
--- tests/bowling_game/test_game.py
+++ tests/bowling_game/test_game.py
@@ -1,24 +1,24 @@
 from src.bowling_game.game import Game
 
-def test_simple_score():
+def test_simple_score() -> None:
     game = Game()
     game.roll(5)
     assert(game.score() == 5)
     
-def test_two_rolls():
+def test_two_rolls() -> None:
     game = Game()
     game.roll(4)
     game.roll(6)
     assert(game.score() == 10)
 
-def test_spare():
+def test_spare() -> None:
     game = Game()
     game.roll(5)
     game.roll(5)
     game.roll(3)
     assert(game.score() == 16)
 
-def test_invalid_spare():
+def test_invalid_spare() -> None:
     game = Game()
     game.roll(4)
     game.roll(5)
@@ -26,14 +26,14 @@ def test_invalid_spare():
     game.roll(3)
     assert(game.score() == 17)
 
-def test_strike():
+def test_strike() -> None:
     game = Game()
     game.roll(10)
     game.roll(5)
     game.roll(3)
     assert(game.score() == 26)
 
-def test_strike_like_spare():
+def test_strike_like_spare() -> None:
     game = Game()
     game.roll(0)
     game.roll(10)
