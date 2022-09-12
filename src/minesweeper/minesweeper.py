7eca26d454b109607ef2f00dade2f27bae34b702
austin-swertfager <62552448+austin-swertfager@users.noreply.github.com>
2022-09-12 16:37:16 -0400

    Working bomb inserts.

--- src/minesweeper/minesweeper.py
+++ src/minesweeper/minesweeper.py
@@ -2,6 +2,7 @@ class Minesweeper:
     def __init__(self, rows: int, columns: int, bombs: list[tuple[int, int]]) -> None:
         self.rows = rows
         self.columns = columns
+        self.bombs = bombs
 
     def display(self) -> str:
         grid = []
@@ -10,5 +11,8 @@ class Minesweeper:
             for j in range(self.columns):
                 inner_grid.append(".")
             grid.append(inner_grid)
+
+        for (row,column) in self.bombs:
+            grid[row][column] = "*"
 
         return "\n".join(["".join(row) for row in grid])
--- tests/minesweeper/test_minesweeper.py
+++ tests/minesweeper/test_minesweeper.py
@@ -4,3 +4,7 @@ from src.minesweeper.minesweeper import Minesweeper
 def test_minesweeper_display() -> None:
     minesweeper = Minesweeper(4, 4, [])
     assert minesweeper.display() == "....\n....\n....\n...."
+
+def test_bomb_insert() -> None:
+    minesweeper = Minesweeper(4, 4, [(1,1)])
+    assert minesweeper.display() == "....\n.*..\n....\n...."
