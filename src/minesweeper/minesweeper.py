51a6f08731166da247d76bd7ffd0e56d15b4f7fd
austin-swertfager <62552448+austin-swertfager@users.noreply.github.com>
2022-09-20 16:13:04 -0400

    Iteration 3 done!
    Co-authored-by: Grégoire Chauvet <gregoirechauvet@users.noreply.github.com>

--- src/minesweeper/minesweeper.py
+++ src/minesweeper/minesweeper.py
@@ -1,26 +1,65 @@
+class GameOver(Exception):
+    pass
+
+
+class WinningGame(Exception):
+    pass
+
+
 class Minesweeper:
     def __init__(self, rows: int, columns: int, bombs: list[tuple[int, int]]) -> None:
-        self.rows = rows
-        self.columns = columns
-        self.bombs = bombs
+        self._rows = rows
+        self._columns = columns
+        self._bombs = bombs
+        self._grid = self._build_grid("0")
+        self._set_bombs()
+        self._moves_grid = self._build_grid(".")
+
+    # This is a method (not a function) because it's within a class
+    def _build_grid(self, char: str) -> list[list[str]]:
+        grid = []
+        for i in range(self._rows):
+            inner_grid = []
+            for j in range(self._columns):
+                inner_grid.append(char)
+            grid.append(inner_grid)
+        return grid
+
+    def _set_bombs(self) -> None:
+        for (row, column) in self._bombs:
+            self._grid[row][column] = "*"
+            for i in range(row - 1, row + 2):
+                for j in range(column - 1, column + 2):
+                    if (
+                        i > self._rows - 1
+                        or i < 0
+                        or j > self._columns - 1
+                        or j < 0
+                        or self._grid[i][j] == "*"
+                    ):
+                        continue
+                    temp_int = int(self._grid[i][j])
+                    temp_int += 1
+                    self._grid[i][j] = str(temp_int)
+
+    def _print(self, grid: list[list[str]]) -> str:
+        return "\n".join(["".join(row) for row in grid])
 
     def display(self) -> str:
-        grid = []
-        for i in range(self.rows):
-            inner_grid = []
-            for j in range(self.columns):
-                inner_grid.append("0")
-            grid.append(inner_grid)
+        return self._print(self._grid)
 
-        for (row,column) in self.bombs:
-            grid[row][column] = "*"
-            for i in range(row-1,row+2):
-                for j in range(column-1,column+2):
-                    if i > self.rows-1 or i < 0 or j > self.columns-1 or j < 0 or grid[i][j] == "*":
-                        continue
-                    temp_int = int(grid[i][j])
-                    temp_int += 1
-                    grid[i][j] = str(temp_int)
+    def play(self, row: int, column: int) -> str:
+        self._moves_grid[row][column] = self._grid[row][column]
 
+        if self._grid[row][column] == "*":
+            raise GameOver()
 
-        return "\n".join(["".join(row) for row in grid])
+        count_unswept = 0
+        for (i, moves_row) in enumerate(self._moves_grid):
+            for (j, moves_cell) in enumerate(moves_row):
+                if moves_cell == ".":
+                    count_unswept += 1
+        if count_unswept == len(self._bombs):
+            raise WinningGame()
+
+        return self._print(self._moves_grid)
--- tests/minesweeper/test_minesweeper.py
+++ tests/minesweeper/test_minesweeper.py
@@ -1,18 +1,49 @@
-from src.minesweeper.minesweeper import Minesweeper
+import pytest
+from src.minesweeper.minesweeper import Minesweeper, GameOver, WinningGame
 
 
 def test_minesweeper_display() -> None:
     minesweeper = Minesweeper(4, 4, [])
     assert minesweeper.display() == "0000\n0000\n0000\n0000"
 
+
 def test_bomb_insert() -> None:
-    minesweeper = Minesweeper(4, 4, [(0,0)])
-    assert minesweeper.display() == "*100\n1100\n0000\n0000" 
+    minesweeper = Minesweeper(4, 4, [(0, 0)])
+    assert minesweeper.display() == "*100\n1100\n0000\n0000"
+
 
 def test_bomb_insert_1() -> None:
-    minesweeper = Minesweeper(4, 4, [(3,3)])
+    minesweeper = Minesweeper(4, 4, [(3, 3)])
     assert minesweeper.display() == "0000\n0000\n0011\n001*"
 
+
 def test_bomb_insert_2() -> None:
-    minesweeper = Minesweeper(4, 4, [(3,3),(2,2)])
+    minesweeper = Minesweeper(4, 4, [(3, 3), (2, 2)])
     assert minesweeper.display() == "0000\n0111\n01*2\n012*"
+
+
+def test_play() -> None:
+    minesweeper = Minesweeper(4, 4, [(0, 0)])
+    assert minesweeper.play(0, 1) == ".1..\n....\n....\n...."
+    assert minesweeper.play(0, 2) == ".10.\n....\n....\n...."
+
+
+def test_game_over() -> None:
+    minesweeper = Minesweeper(4, 4, [(0, 0)])
+    with pytest.raises(GameOver):
+        minesweeper.play(0, 0)
+
+
+def test_win_game() -> None:
+    minesweeper = Minesweeper(2, 2, [(0, 0)])
+    minesweeper.play(0, 1)
+    minesweeper.play(1, 0)
+    with pytest.raises(WinningGame):
+        minesweeper.play(1, 1)
+
+def test_almost_win_game() -> None:
+    minesweeper = Minesweeper(2, 2, [(0, 0)])
+    minesweeper.play(0, 1)
+    minesweeper.play(1, 0)
+    with pytest.raises(GameOver):
+        minesweeper.play(0, 0)
