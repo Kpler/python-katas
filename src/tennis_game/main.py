025ae0ba2cd0842dc7cda4eb768d0bb027e453d6
Namsam <snaim@kpler.com>
2022-09-28 11:44:50 +0200

    handle win condition

--- src/tennis_game/main.py
+++ src/tennis_game/main.py
@@ -1,15 +1,14 @@
 class Game(object):
 
-
     score_list = ["love", "15", "30", "40"]
-
 
     def __init__(self):
         self.game_score = [0, 0]
-
 
     def score(self):
         return f"{self.score_list[self.game_score[0]]}-{self.score_list[self.game_score[1]]}"
 
     def score_point(self, player_number):
-        self.game_score[player_number]+= 1
+        self.game_score[player_number] += 1
+        if self.game_score[player_number] > len(self.score_list) - 1:
+            raise Exception(f"Player {player_number + 1} wins !")
--- tests/tennis_game/test_tennis_game.py
+++ tests/tennis_game/test_tennis_game.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.tennis_game.main import Game
 
 PLAYER_1 = 0
@@ -15,3 +17,26 @@ def test_game_score_updates():
     game.score_point(PLAYER_1)
 
     assert game.score() == "15-love"
+
+
+def test_game_win_player_1():
+    game = Game()
+    game.score_point(PLAYER_1)
+    game.score_point(PLAYER_1)
+    game.score_point(PLAYER_2)
+    game.score_point(PLAYER_1)
+
+    with pytest.raises(Exception):
+        game.score_point(PLAYER_1)
+
+
+def test_game_deuce():
+    game = Game()
+    game.score_point(PLAYER_1)
+    game.score_point(PLAYER_2)
+    game.score_point(PLAYER_1)
+    game.score_point(PLAYER_2)
+    game.score_point(PLAYER_2)
+    game.score_point(PLAYER_1)
+
+    assert game.score() == "deuce"
