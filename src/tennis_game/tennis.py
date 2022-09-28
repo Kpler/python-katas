45e473c04ec6cb4dcb68778a6178480f9c991227
Nicolas Guignard <nicolas065@gmail.com>
2022-09-28 11:52:53 +0200

    make tests pass refactor

--- src/tennis_game/tennis.py
+++ src/tennis_game/tennis.py
@@ -1,17 +1,34 @@
-class Tennis:
+from enum import Enum
 
+
+class Player(Enum):
+    ONE = 'one'
+    TWO = 'two'
+
+
+class Tennis:
     current_player: str
     game_score: dict
+    match_score: dict
 
     def __init__(self) -> None:
-        self.game_score = { "one": 0, "two": 0 }
+        self.game_score = {Player.ONE: 0, Player.TWO: 0}
+        self.match_score = {Player.ONE: 0, Player.TWO: 0}
 
-    def play(self, winner: str): # TODO one/two
+    def play(self, winner: Player):
         current_winner_score = self.game_score.get(winner)
-
         if current_winner_score == 0:
             self.game_score[winner] = 15
+        elif current_winner_score == 15:
+            self.game_score[winner] = 30
+        elif current_winner_score == 30:
+            self.game_score[winner] = 40
+        elif current_winner_score == 40:
+            self.game_score[winner] = 0
+            self.match_score[winner] += 1
 
-
-    def get_score(self):
+    def get_game_score(self):
         return self.game_score
+
+    def get_match_score(self):
+        return self.match_score
--- tests/tennis_game/tennis.py
+++ tests/tennis_game/tennis.py
@@ -1,47 +1,51 @@
 import pytest
 
-from src.tennis_game.tennis import Tennis
+from src.tennis_game.tennis import Tennis, Player
 
 
 def test_initial_score_should_be_zero():
   game = Tennis()
   score = game.get_game_score()
-  assert score.get("one") == 0
-  assert score.get("two") == 0
+  assert score.get(Player.ONE) == 0
+  assert score.get(Player.TWO) == 0
 
 
 def test_play_should_increment_score():
   game = Tennis()
 
-  game.play('one')
+  game.play(Player.ONE)
   score = game.get_game_score()
 
-  assert score.get("one") == 15
-  assert score.get("two") == 0
+  assert score.get(Player.ONE) == 15
+  assert score.get(Player.TWO) == 0
 
-# def test_game_score_should_reset_at_end_of_game():
-#   game = Tennis()
-
-#   game.play('one')
-#   game.play('one')
-#   game.play('one')
-#   game.play('one')
-#   score = game.get_game_score()
-
-#   assert score.get("one") == 0
-#   assert score.get("two") == 0
 
 def test_game_score_should_increment_after_each_point():
   game = Tennis()
 
-  game.play('one')
+  game.play(Player.ONE)
   score = game.get_game_score()
-  assert score.get("one") == 15
+  assert score.get(Player.ONE) == 15
 
-  game.play('one')
+  game.play(Player.ONE)
   score = game.get_game_score()
-  assert score.get("one") == 30
+  assert score.get(Player.ONE) == 30
 
-  game.play('one')
+  game.play(Player.ONE)
   score = game.get_game_score()
-  assert score.get("one") == 40
+  assert score.get(Player.ONE) == 40
+
+
+def test_game_score_should_reset_at_end_of_game():
+  game = Tennis()
+
+  game.play(Player.ONE)
+  game.play(Player.ONE)
+  game.play(Player.ONE)
+  game.play(Player.ONE)
+  score = game.get_game_score()
+  match_score = game.get_match_score()
+
+  assert score.get(Player.ONE) == 0
+  assert score.get(Player.TWO) == 0
+  assert match_score == { Player.ONE: 1, Player.TWO: 0 }
