87c87f3fd16e6f3d725fa22f4a8a69080dac9b63
cassiebrooks <cassbrooks@outlook.com>
2022-09-28 09:42:24 +0000

    Add more tests

--- src/tennis_game/tennis.py
+++ src/tennis_game/tennis.py
@@ -4,10 +4,14 @@ class Tennis:
     game_score: dict
 
     def __init__(self) -> None:
-        self.game_score = { 'one': 0, 'two': 0 }
+        self.game_score = { "one": 0, "two": 0 }
 
-    def play(self, winner: str):
-        pass
+    def play(self, winner: str): # TODO one/two
+        current_winner_score = self.game_score.get(winner)
+
+        if current_winner_score == 0:
+            self.game_score[winner] = 15
+
 
     def get_score(self):
         return self.game_score
--- tests/tennis_game/tennis.py
+++ tests/tennis_game/tennis.py
@@ -5,7 +5,7 @@ from src.tennis_game.tennis import Tennis
 
 def test_initial_score_should_be_zero():
   game = Tennis()
-  score = game.get_score()
+  score = game.get_game_score()
   assert score.get("one") == 0
   assert score.get("two") == 0
 
@@ -14,10 +14,34 @@ def test_play_should_increment_score():
   game = Tennis()
 
   game.play('one')
-  score = game.get_score()
+  score = game.get_game_score()
 
   assert score.get("one") == 15
   assert score.get("two") == 0
 
-  
+# def test_game_score_should_reset_at_end_of_game():
+#   game = Tennis()
 
+#   game.play('one')
+#   game.play('one')
+#   game.play('one')
+#   game.play('one')
+#   score = game.get_game_score()
+
+#   assert score.get("one") == 0
+#   assert score.get("two") == 0
+
+def test_game_score_should_increment_after_each_point():
+  game = Tennis()
+
+  game.play('one')
+  score = game.get_game_score()
+  assert score.get("one") == 15
+
+  game.play('one')
+  score = game.get_game_score()
+  assert score.get("one") == 30
+
+  game.play('one')
+  score = game.get_game_score()
+  assert score.get("one") == 40
