79b92274c6f4cd59d43518e3a5c94c123f3b5471
ejaoua <ejaoua@kpler.com>
2022-09-28 11:56:19 +0200

    add game init

--- tests/tennis_game/test_tennis_game.py
+++ tests/tennis_game/test_tennis_game.py
@@ -11,5 +11,12 @@ def test_player_status():
     player.increase_score()
     assert player.get_status() == "40"
 
-def test_is_deuce():
-    pass
+def test_win_without_deuce():
+    game = Game()
+    game.p1.increase_score()
+    game.p1.increase_score()
+    game.p1.increase_score()
+    game.p1.increase_score()
+    assert game.status == 'over'
+    assert game.winner == game.p1
+    assert game.score == [40, 0]
