1c3e5153c026f1c758a0c296423afd841f5b3b6b
DavidPalmi <92101787+DavidPalmi@users.noreply.github.com>
2022-10-04 12:13:51 -0400

    david second commit

--- src/tennis_game/tennis_game.py
+++ src/tennis_game/tennis_game.py
@@ -1,3 +1,28 @@
+from enum import Enum
+
+
+class SCORES (Enum):
+  LOVE = 0
+  FIFTEEN = 15
+  THIRTY = 30
+  FORTY = 40
+
 class Tennis:
-  def get_game_score():
-    pass
+  score_player_1 = SCORES.LOVE
+  score_player_2 = SCORES.LOVE
+  def get_game_score(self):
+    return f"{self.score_player_1.value}-{self.score_player_2.value}"
+
+  def play(self, player: str):
+    if player == "0" and self.score_player_1 == SCORES.LOVE:
+      self.score_player_1 = SCORES.FIFTEEN
+    elif player == "0" and self.score_player_1 == SCORES.FIFTEEN:
+      self.score_player_1 = SCORES.THIRTY
+    elif player == "0" and self.score_player_1 == SCORES.THIRTY:
+      self.score_player_1 = SCORES.FORTY
+    elif player == "1" and self.score_player_2 == SCORES.LOVE:
+      self.score_player_2 = SCORES.FIFTEEN
+    elif player == "1" and self.score_player_2 == SCORES.FIFTEEN:
+      self.score_player_2 = SCORES.THIRTY
+    elif player == "1" and self.score_player_2 == SCORES.THIRTY:
+      self.score_player_2 = SCORES.FORTY
--- tests/tennis_game/test_tennis_game.py
+++ tests/tennis_game/test_tennis_game.py
@@ -4,5 +4,21 @@ from src.tennis_game.tennis_game import Tennis
 def test_initial_score_should_be_zero():
   game = Tennis()
   score = game.get_game_score()
-  assert score == "0"
-  
+  assert score == "0-0"
+
+def test_first_score_should_be_incremeted():
+  game = Tennis()
+  game.play("0")
+  score = game.get_game_score()
+  assert score == "15-0"
+
+def test_score_should_be_incremeted_all_points():
+  game = Tennis()
+  game.play("0")
+  game.play("1")
+  game.play("0")
+  game.play("1")
+  game.play("0")
+  game.play("1")
+  score = game.get_game_score()
+  assert score == "40-40"
