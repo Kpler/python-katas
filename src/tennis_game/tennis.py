8142a5e061d01e6a13c56e74095c95d96261b979
pguardati-kpler <98783908+pguardati-kpler@users.noreply.github.com>
2022-10-05 11:29:18 +0200

    chore: add function to update score

--- src/tennis_game/tennis.py
+++ src/tennis_game/tennis.py
@@ -2,14 +2,19 @@ class Game:
 
     player_1_score = 0
     player_2_score = 0
-    score_labels = ['love', '15', '30', '40']
+    score_labels = ["love", "15", "30", "40"]
 
     def __int__(self):
         pass
+
+    def player_1_scored(self):
+        self.player_1_score += 1
+
+    def player_2_scored(self):
+        self.player_2_score += 1
 
     def is_finished(self):
         return False
 
     def get_score(self):
         return f"{self.score_labels[self.player_1_score]}-{self.score_labels[self.player_2_score]}"
-
--- tests/tennis_game/test_tennis.py
+++ tests/tennis_game/test_tennis.py
@@ -4,9 +4,11 @@ from src.tennis_game.tennis import Game
 def test_create_game_starts_at_love_love():
     game = Game()
     assert not game.is_finished()
-    assert game.get_score() == 'love-love'
+    assert game.get_score() == "love-love"
 
-# def test_create_game_starts_at_love_love():
-#     game = Game()
-#     assert not game.is_finished()
-#     assert game.get_score() == 'love-love'
+
+def test_update_player_score():
+    game = Game()
+    game.player_1_scored()
+    assert not game.is_finished()
+    assert game.get_score() == "15-love"
