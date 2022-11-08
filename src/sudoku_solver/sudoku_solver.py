a505107598cf6d128ec0413063e6ca20d758f9c9
austin-swertfager <62552448+austin-swertfager@users.noreply.github.com>
2022-11-08 17:30:00 -0500

    Working version of sudoku solver! Boo ya kasha

--- src/sudoku_solver/sudoku_solver.py
+++ src/sudoku_solver/sudoku_solver.py
@@ -2,17 +2,23 @@ from typing import Optional
 
 Grid = list[list[Optional[int]]]
 
+
+class NoSolution(Exception):
+    pass
+
+
 def find_row_values(grid: Grid, row_index: int) -> set[int]:
     return set([value for value in grid[row_index] if value is not None])
+
 
 def find_column_values(grid: Grid, column_index: int) -> set[int]:
     return set([value for row in grid if (value := row[column_index]) is not None])
 
-def find_square_values(grid: Grid, row_index: int,column_index: int) -> set[int]:
+
+def find_square_values(grid: Grid, row_index: int, column_index: int) -> set[int]:
     square_row_index_start = row_index - (row_index % 3)
     square_column_index_start = column_index - (column_index % 3)
 
-    print(row_index, column_index, square_row_index_start, square_column_index_start)
     output: set[int] = set()
     for i in range(square_row_index_start, square_row_index_start + 3):
         for j in range(square_column_index_start, square_column_index_start + 3):
@@ -21,22 +27,42 @@ def find_square_values(grid: Grid, row_index: int,column_index: int) -> set[int]
                 output.add(value)
     return output
 
-def find_possible_values(grid: Grid, row_index: int,column_index: int) -> list[int]:
+
+def find_possible_values(grid: Grid, row_index: int, column_index: int) -> list[int]:
     possiblities = set(range(1, 10))
-    row_values = find_row_values(grid,row_index)
-    column_values = find_column_values(grid,column_index)
-    square_values = find_square_values(grid,row_index,column_index)
+    row_values = find_row_values(grid, row_index)
+    column_values = find_column_values(grid, column_index)
+    square_values = find_square_values(grid, row_index, column_index)
     possible_values = possiblities - row_values - column_values - square_values
     return list(possible_values)
 
-def solve(input_grid: Grid) -> Grid:
-    output_grid = [[cell for cell in row] for row in input_grid]
+
+def solve(input_grid: Grid) -> None:
     for row_index, row in enumerate(input_grid):
         for cell_index, cell in enumerate(row):
             if cell != None:
                 continue
-            else:
-                possible_values = find_possible_values(input_grid,row_index,cell_index)
-                if len(possible_values) == 1:
-                    output_grid[row_index][cell_index] = possible_values[0]
-    return output_grid
+
+            possible_values = find_possible_values(input_grid, row_index, cell_index)
+            # print(f"POSSIBLE: {possible_values}")
+            if len(possible_values) == 0:
+                raise NoSolution()
+
+            index = 0
+            solution_found = False
+            while not solution_found:
+                # print(f"Trying {possible_values[index]} ({index}) on {row_index}/{cell_index}")
+                # print(input_grid)
+                # print("================")
+                input_grid[row_index][cell_index] = possible_values[index]
+                try:
+                    solve(input_grid)
+                except NoSolution:
+                    # print("NO SOLUTION CAUGHT")
+                    if index + 2 > len(possible_values):
+                        # print(f"NO SOLUTION for {row_index}/{cell_index}")
+                        input_grid[row_index][cell_index] = None
+                        raise NoSolution()
+                    index += 1
+                else:
+                    solution_found = True
