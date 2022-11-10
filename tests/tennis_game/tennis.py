6b037f26d295deae139033e8fd5ec5e478a10065
Nicolas Guignard <nicolas065@gmail.com>
2022-11-10 12:16:26 +0100

    init exercise

--- tests/tennis_game/tennis.py
+++ tests/tennis_game/tennis.py
@@ -11,41 +11,5 @@ def test_initial_score_should_be_zero():
 
 
 def test_play_should_increment_score():
-  game = Tennis()
+  pass
 
-  game.play(Player.ONE)
-  score = game.get_game_score()
-
-  assert score.get(Player.ONE) == 15
-  assert score.get(Player.TWO) == 0
-
-
-def test_game_score_should_increment_after_each_point():
-  game = Tennis()
-
-  game.play(Player.ONE)
-  score = game.get_game_score()
-  assert score.get(Player.ONE) == 15
-
-  game.play(Player.ONE)
-  score = game.get_game_score()
-  assert score.get(Player.ONE) == 30
-
-  game.play(Player.ONE)
-  score = game.get_game_score()
-  assert score.get(Player.ONE) == 40
-
-
-def test_game_score_should_reset_at_end_of_game():
-  game = Tennis()
-
-  game.play(Player.ONE)
-  game.play(Player.ONE)
-  game.play(Player.ONE)
-  game.play(Player.ONE)
-  score = game.get_game_score()
-  match_score = game.get_match_score()
-
-  assert score.get(Player.ONE) == 0
-  assert score.get(Player.TWO) == 0
-  assert match_score == { Player.ONE: 1, Player.TWO: 0 }
