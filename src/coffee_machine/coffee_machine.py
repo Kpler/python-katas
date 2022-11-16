10a8fcd780be467365bd6f508358fc50fe0cfe8f
Gary <me@garyfibiger.cz>
2022-11-16 11:08:09 +0100

    feat(coffee): make sugars work

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -3,9 +3,9 @@ from enum import Enum
 
 
 class Drink(Enum):
-    TEA = 'tea'
-    COFFEE = 'coffee'
-    HOT_CHOCOLATE = 'hot chocolate'
+    TEA = "tea"
+    COFFEE = "coffee"
+    HOT_CHOCOLATE = "hot chocolate"
 
 
 @dataclasses.dataclass
@@ -15,26 +15,27 @@ class Order:
     stick: bool
 
 
-
 class CoffeeMachine:
 
     DRINK_MATCHING = {
-        'T': Drink.TEA,
-        'C': Drink.COFFEE,
-        'H': Drink.HOT_CHOCOLATE,
+        "T": Drink.TEA,
+        "C": Drink.COFFEE,
+        "H": Drink.HOT_CHOCOLATE,
     }
 
-    def process(self, order: str) -> str:
-        return self.parser(order)
+    def process(self, order_input: str) -> str:
+        order = self.parser(order_input)
+        output = self.output_generator(order)
+        return output
 
     def parser(self, order: str) -> Order:
         drink, sugars, _ = order.split(":")
 
         return Order(
-            drink=self.DRINK_MATCHING[drink],
-            sugars=0 if sugars is None else sugars,
-            stick=False
+            drink=self.DRINK_MATCHING[drink], sugars=0 if sugars is "" else int(sugars), stick=False
         )
 
     def output_generator(self, order: Order) -> str:
-        pass
+        if order.sugars == 0:
+            return f"Drink maker makes 1 {order.drink.value} with no sugar - and therefore no stick"
+        return f"Drink maker makes 1 {order.drink.value} with {order.sugars} sugar{'s' if order.sugars > 1 else ''} and a stick"
--- tests/coffee_machine/test_coffee_machine.py
+++ tests/coffee_machine/test_coffee_machine.py
@@ -18,14 +18,15 @@ def test_coffee_machine_without_sugar(machine_input, expected_output):
     assert machine_output == expected_output
 
 
-# @pytest.mark.parametrize(
-#     ["machine_input", "expected_output"],
-#     [
-#         ("T:1:0", "Drink maker makes 1 chocolate with no sugar - and therefore no stick"),
-#     ],
-# )
-# def test_coffee_machine_with_sugar(machine_input, expected_output):
-#     machine = CoffeeMachine()
-#     machine_output = machine.process(machine_input)
-#
-#     assert machine_output == expected_output
+@pytest.mark.parametrize(
+    ["machine_input", "expected_output"],
+    [
+        ("T:1:0", "Drink maker makes 1 tea with 1 sugar and a stick"),
+        ("T:2:0", "Drink maker makes 1 tea with 2 sugars and a stick"),
+    ],
+)
+def test_coffee_machine_with_sugar(machine_input, expected_output):
+    machine = CoffeeMachine()
+    machine_output = machine.process(machine_input)
+
+    assert machine_output == expected_output
