7c122704709ea636af887b9399f5302ef7635196
Pierre Karpov <karpovpierre@gmail.com>
2022-11-16 17:38:44 +0800

    serve coffee and chos

---
+++ src/coffee_machine/coffee_machine.py
@@ -0,0 +1,7 @@
+class CoffeeMachine:
+    def make_tea(self) -> str:
+        return "T::"
+    def make_coffee(self):
+        return "C::"
+    def make_chocolate(self):
+        return "H::"
--- tests/coffee_machine/coffee_machine_test.py
+++ tests/coffee_machine/coffee_machine_test.py
@@ -1,16 +1,23 @@
 import unittest
 
-
-class CoffeeMachine:
-    def make_tea(self) -> str:
-        return "T::"
+from src.coffee_machine.coffee_machine import CoffeeMachine
 
 
 class CoffeeMachineTestCase(unittest.TestCase):
-    def test_when_making_tea_then_get_tea_1_0(self):
+    def test_when_making_tea_then_get_t_colon_colon(self):
         coffee_machine = CoffeeMachine()
         result = coffee_machine.make_tea()
-        self.assertEqual(result, "T::")  # add assertion here
+        self.assertEqual(result, "T::")
+
+    def test_when_making_coffee_then_get_c_colon_colon(self):
+        coffee_machine = CoffeeMachine()
+        result = coffee_machine.make_coffee()
+        self.assertEqual(result, "C::")
+
+    def test_when_making_chocolate_then_get_h_colon_colon(self):
+        coffee_machine = CoffeeMachine()
+        result = coffee_machine.make_chocolate()
+        self.assertEqual(result, "H::")
 
 
 if __name__ == "__main__":
