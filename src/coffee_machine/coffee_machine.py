d7b3445201583db8a2cc4772ea7a877baecc3c1f
Namsam <snaim@kpler.com>
2022-11-16 10:55:34 +0100

    improve parser + add output method

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -15,10 +15,26 @@ class Order:
     stick: bool
 
 
+
 class CoffeeMachine:
+
+    DRINK_MATCHING = {
+        'T': Drink.TEA,
+        'C': Drink.COFFEE,
+        'H': Drink.HOT_CHOCOLATE,
+    }
+
     def process(self, order: str) -> str:
-        return order
+        return self.parser(order)
 
     def parser(self, order: str) -> Order:
         drink, sugars, _ = order.split(":")
-        return Order(drink=drink, sugars=sugars, stick=False)
+
+        return Order(
+            drink=self.DRINK_MATCHING[drink],
+            sugars=0 if sugars is None else sugars,
+            stick=False
+        )
+
+    def output_generator(self, order: Order) -> str:
+        pass
