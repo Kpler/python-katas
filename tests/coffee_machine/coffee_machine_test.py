19637922007604ae7e39977fc34e47535cdb9a57
logeshkpler <lselvaraj@kpler.com>
2022-11-16 15:00:59 +0530

    make coffe method updated

--- tests/coffee_machine/coffee_machine_test.py
+++ tests/coffee_machine/coffee_machine_test.py
@@ -1,17 +1,17 @@
 import unittest
 
 
-class CoffeMachine:
-    pass
+class CoffeeMachine:
+    def make_tea(self) -> str:
+        return "T::"
 
 
 class CoffeeMachineTestCase(unittest.TestCase):
-    coffee_machine = CoffeMachine()
+    def test_when_making_tea_then_get_tea_1_0(self):
+        coffee_machine = CoffeeMachine()
+        result = coffee_machine.make_tea()
+        self.assertEqual(result, "T::")  # add assertion here
 
-    def when_making_tea_then_get_tea_1_0(self):
-        coffee_machine.make_tea()
-        self.assertEqual(True, True)  # add assertion here
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
