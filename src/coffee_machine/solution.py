c663488e5639f2ebfbe416011b46533194aee227
Benjamin Mironer <bmironer@kpler.com>
2022-11-17 17:34:36 +0100

    added command enum

--- src/coffee_machine/solution.py
+++ src/coffee_machine/solution.py
@@ -1,12 +1,24 @@
 
-def drink_maker(drink_type:str, sugar_amt:int) -> str: 
+from enum import Enum
+
+class CommandType(Enum):
+    TEA = "T"
+    COFFEE = "C"
+    CHOCOLATE = "H"
+
+
+# how do we handle bad sugar inputs?
+def drink_maker(command:CommandType, sugar_amt:int) -> str: 
     '''
     '''
-    return f"{drink_type}:{sugar_amt}:{'0' if sugar_amt>0 else ''}"
+    return f"{command.value}:{sugar_amt}:{'0' if sugar_amt>0 else ''}"
 
 
-def test_drink_maker():
-
-    assert drink_maker('C', 1) == 'C:1:0'
+def test_drink_maker_coffee_1_sugar():
+    assert drink_maker(CommandType.COFFEE, 1) == 'C:1:0'
     
-    
+def test_drink_maker_no_sugar():
+    assert drink_maker(CommandType.COFFEE, 0) == 'C:0:'
+
+def test_drink_maker_no_sugar():
+    assert drink_maker(CommandType.COFFEE, -1) == 'C:0:'
