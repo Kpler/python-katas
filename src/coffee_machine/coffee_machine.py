724149e06213f3ddf90ae1529532a2f03c7a8692
guillaumerevillon <grevillon@kpler.com>
2022-11-17 17:47:14 +0100

    feat: add price to order

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -6,9 +6,18 @@ class DrinkType(Enum):
     TEA = 'T'
     HOT_CHOCOLATE = 'H'
 
+    def price(self) -> float:
+        if self.COFFEE:
+            return 0.5
+        elif self.TEA:
+            return 0.4
+        else:
+            return 0.6
+
 @dataclasses.dataclass
 class Order:
     drink_type: DrinkType
+    money: float = 0.0
     sugars: int = 0
 
 
@@ -16,4 +25,7 @@ class CoffeeMachine:
     def generate_order(self, order: Order) -> str:
         sugars_str = "" if order.sugars == 0 else order.sugars.__str__()
         stick = "" if order.sugars == 0 else "0"
+        if order.money < order.drink_type.price():
+            return "M:message-content"
+
         return f"{order.drink_type.value}:{sugars_str}:{stick}"
--- tests/coffee_machine/test_coffee_machine.py
+++ tests/coffee_machine/test_coffee_machine.py
@@ -6,7 +6,7 @@ from src.coffee_machine.coffee_machine import CoffeeMachine, Order, DrinkType
 def test_we_order_coffee():
     expected_order= "C::"
     drink_type = DrinkType.COFFEE
-    order_input = Order(drink_type=drink_type)
+    order_input = Order(drink_type=drink_type, money=1)
     coffee_machine = CoffeeMachine()
     result = coffee_machine.generate_order(order_input)
     assert result == expected_order
@@ -15,7 +15,7 @@ def test_we_order_coffee():
 def test_we_order_tea():
     expected_order= "T::"
     drink_type = DrinkType.TEA
-    order_input = Order(drink_type=drink_type)
+    order_input = Order(drink_type=drink_type, money=1)
     coffee_machine = CoffeeMachine()
     result = coffee_machine.generate_order(order_input)
     assert result == expected_order
@@ -24,7 +24,7 @@ def test_we_order_tea():
 def test_we_order_hot_chocolate():
     expected_order= "H::"
     drink_type = DrinkType.HOT_CHOCOLATE
-    order_input = Order(drink_type=drink_type)
+    order_input = Order(drink_type=drink_type, money=1)
     coffee_machine = CoffeeMachine()
     result = coffee_machine.generate_order(order_input)
     assert result == expected_order
@@ -33,7 +33,7 @@ def test_we_order_hot_chocolate():
 def test_we_order_drink_and_sugars_properly():
     expected_order= "H:2:0"
     drink_type = DrinkType.HOT_CHOCOLATE
-    order_input = Order(drink_type=drink_type, sugars=2)
+    order_input = Order(drink_type=drink_type, sugars=2, money=1)
     coffee_machine = CoffeeMachine()
     result = coffee_machine.generate_order(order_input)
     assert result == expected_order
@@ -43,6 +43,15 @@ def test_order_coffee_with_money():
     expected_result = "M:message-content"
     drink_type = DrinkType.HOT_CHOCOLATE
     order_input = Order(drink_type=drink_type, sugars=2)
+    coffee_machine = CoffeeMachine()
+    result = coffee_machine.generate_order(order_input)
+    assert result == expected_result
+
+
+def test_order_coffee_with_not_enough_money():
+    expected_result = "M:message-content"
+    drink_type = DrinkType.HOT_CHOCOLATE
+    order_input = Order(drink_type=drink_type, sugars=2, money=0.4)
     coffee_machine = CoffeeMachine()
     result = coffee_machine.generate_order(order_input)
     assert result == expected_result
