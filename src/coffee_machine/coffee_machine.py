5135bd8ea971dc955f5d29b60f03e9ef41aff0ce
etienne dhaussy <edhaussy@gmail.com>
2022-11-17 17:41:22 +0100

    thune

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -13,3 +13,9 @@ def make_order(type: DrinkType, number_of_sugar: int) -> str:
     sugar_string, stick_string = ("", "") if is_no_sugar else (str(number_of_sugar), "0")
 
     return f"{type.value}:{sugar_string}:{stick_string}"
+
+
+def create_order(drink_type: DrinkType, number_of_sugar: int,
+                 thune: float) -> str:
+    if thune==0:
+        return "M:not enough_cash"
