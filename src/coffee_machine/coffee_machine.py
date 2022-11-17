358415b1b2e009330cb50125a5ca54fedae7bee9
Henry Nomichith <hnomichith@kpler.com>
2022-11-17 17:47:02 +0100

    plof plouf plaf

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -1,9 +1,21 @@
 import enum
 
+
 class DrinkType(enum.Enum):
     COFFEE: str = "C"
     TEA: str = "T"
     HOT_CHOCOLATE: str = "H"
+
+    def get_price(self) -> float:
+        match self:
+            case DrinkType.COFFEE:
+                return 0.6
+            case DrinkType.TEA:
+                return 0.4
+            case DrinkType.HOT_CHOCOLATE:
+                return 0.5
+            case _:
+                raise ValueError("Can't resolve price from drink type")
 
 
 def make_order(type: DrinkType, number_of_sugar: int) -> str:
@@ -15,7 +27,12 @@ def make_order(type: DrinkType, number_of_sugar: int) -> str:
     return f"{type.value}:{sugar_string}:{stick_string}"
 
 
-def create_order(drink_type: DrinkType, number_of_sugar: int,
-                 thune: float) -> str:
-    if thune==0:
+def create_order(
+        drink_type: DrinkType,
+        number_of_sugar: int,
+        thune: float
+) -> str:
+    if thune < drink_type.get_price():
         return "M:not enough_cash"
+    else:
+        return make_order(drink_type, number_of_sugar)
--- tests/coffee_machine/test_coffee_machine.py
+++ tests/coffee_machine/test_coffee_machine.py
@@ -13,6 +13,10 @@ def test_not_make_hot_chocolate():
     order = make_order(DrinkType.TEA, 0)
     assert not order == "H::"
 
-def test_create_order():
-    assert create_order(drink_type=DrinkType.TEA, number_of_sugar=7,
-                        thune=0) == "M:not enough_cash"
+def test_not_enough_cash():
+    order = create_order(drink_type=DrinkType.TEA, number_of_sugar=7,thune=0)
+    assert order == "M:not enough_cash"
+
+def test_enough_cash():
+    order = create_order(drink_type=DrinkType.TEA, number_of_sugar=7, thune=0.4)
+    assert order == "T:7:0"
