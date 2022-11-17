478126f6c01429e2a973fd3afc0474da948d0dd3
ArthurVanier <arthur.vanier@epitech.eu>
2022-11-17 17:20:55 +0100

    arthur

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -1,7 +1,11 @@
 
 def make_order(type: str, number_of_sugar: int) -> str:
 
-    sugar_string = "" if number_of_sugar < 1 else str(number_of_sugar)
-    stick_string = "0" if  number_of_sugar >= 1 else ""
+    is_no_sugar = number_of_sugar < 1
+
+    sugar_string, stick_string = ("", "") if is_no_sugar else (str(number_of_sugar), "0")
 
     return f"{type}:{sugar_string}:{stick_string}"
+
+
+class drink_type(enumerate):
