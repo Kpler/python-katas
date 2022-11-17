6a3cfc119a4b36aa7463d8d3b0ad263e4cdbfcb1
etienne dhaussy <edhaussy@gmail.com>
2022-11-17 17:40:37 +0100

    thune

--- tests/coffee_machine/test_coffee_machine.py
+++ tests/coffee_machine/test_coffee_machine.py
@@ -1,4 +1,5 @@
-from src.coffee_machine.coffee_machine import make_order, DrinkType
+from src.coffee_machine.coffee_machine import make_order, DrinkType, create_order
+
 
 def test_make_tea():
     order = make_order(DrinkType.TEA, 1)
@@ -11,3 +12,7 @@ def test_make_hot_chocolate():
 def test_not_make_hot_chocolate():
     order = make_order(DrinkType.TEA, 0)
     assert not order == "H::"
+
+def test_create_order():
+    assert create_order(drink_type=DrinkType.TEA, number_of_sugar=7,
+                        thune=0) == "M:not enough_cash"
