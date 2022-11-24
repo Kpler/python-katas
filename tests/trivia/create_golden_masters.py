8cd7b3b3d99209f44a2b39805be7801d75a40655
Nicolas Guignard <nicolas065@gmail.com>
2022-11-24 18:33:41 +0800

    WIP

--- tests/trivia/create_golden_masters.py
+++ tests/trivia/create_golden_masters.py
@@ -5,14 +5,12 @@ from contextlib import redirect_stdout
 DIRECTORY = "golden_masters"
 
 if __name__ == '__main__':
-    f = io.StringIO()
     seeds = [237, 345, 42]
 
     for seed in seeds:
+        f = io.StringIO()
         with redirect_stdout(f):
             main(seed)
         out = f.getvalue()
         with open(f"{DIRECTORY}/seed_{seed}.txt", "w") as e:
             e.write(out)
-
-
--- tests/trivia/test_trivia.py
+++ tests/trivia/test_trivia.py
@@ -1,13 +1,14 @@
+import pytest as pytest
+
 from src.trivia.trivia import main
 import io
 from contextlib import redirect_stdout
 
 DIRECTORY = "golden_masters"
+seeds = [237, 345, 42]
 
 if __name__ == '__main__':
     f = io.StringIO()
-    seeds = [237, 345, 42]
-    
     for seed in seeds:
         with open(f"{DIRECTORY}/seed_{seed}.txt", "r") as e:
             fff = e.read()
@@ -16,4 +17,16 @@ if __name__ == '__main__':
             main(seed)
         out = f.getvalue()
 
+
+@pytest.mark.parametrize("seed", seeds)
+def test_trivia_seeds(seed):
+    f = io.StringIO()
+    with redirect_stdout(f):
+        main(seed)
+    result = f.getvalue()
+
+    with open(f"{DIRECTORY}/seed_{seed}.txt", "r") as file:
+        expected = file.read()
+
+    assert result == expected
 
