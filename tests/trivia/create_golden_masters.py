dc33628e7eb98cf3754af75d16a583bf08535a0e
Léo <ldureuil@kpler.com>
2022-11-24 18:21:01 +0800

    leo

--- tests/trivia/create_golden_masters.py
+++ tests/trivia/create_golden_masters.py
@@ -2,10 +2,17 @@ from src.trivia.trivia import main
 import io
 from contextlib import redirect_stdout
 
+DIRECTORY = "golden_masters"
 
 if __name__ == '__main__':
     f = io.StringIO()
-    with redirect_stdout(f):
-        main(237)
-    out = f.getvalue()
+    seeds = [237, 345, 42]
+
+    for seed in seeds:
+        with redirect_stdout(f):
+            main(seed)
+        out = f.getvalue()
+        with open(f"{DIRECTORY}/seed_{seed}.txt", "w") as e:
+            e.write(out)
+
 
