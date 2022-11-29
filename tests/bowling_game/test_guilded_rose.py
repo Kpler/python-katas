a9eeaf78d2ffdba8cc582588dac68406ca581703
Nicolas Guignard <nicolas065@gmail.com>
2022-11-29 21:18:16 +0800

    refactor test name add combination capability to test

--- tests/bowling_game/test_guilded_rose.py
+++ tests/bowling_game/test_guilded_rose.py
@@ -1,12 +1,21 @@
-from approvaltests.approvals import verify
+from approvaltests.combination_approvals import verify_all_combinations
 
 from src.gilded_rose.guilded_rose import Item, GildedRose
 
 
-def test_approval():
-    items = [
-        Item(name='Aged Brie', sell_in=1, quality=1),
+def test_gilded_rose_approval_tests():
+    verify_all_combinations(
+        execute_update_quality,
+        [['Aged Brie'],
+        [1],
+        [1]]
+    )
+
+
+def execute_update_quality(name: str, sell_in: int, quality: int) -> Item:
+    items_under_test = [
+        Item(name=name, sell_in=sell_in, quality=quality),
     ]
-    gilded_rose = GildedRose(items)
+    gilded_rose = GildedRose(items_under_test)
     gilded_rose.update_quality()
-    verify(gilded_rose.items)
+    return gilded_rose.items[0]
