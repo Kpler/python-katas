db540d014fabd5a49daa8ff1c55abd8c47b0b99c
Cassie Brooks <cassbrooks@outlook.com>
2022-12-08 11:17:25 +0100

    everything is broken

--- src/tennis_game/tennis_tdd_test.py
+++ src/tennis_game/tennis_tdd_test.py
@@ -2,11 +2,10 @@ import pytest
 from dataclasses import dataclass
 
 # implementation here
-
-
 @dataclass()
 class Player:
   score: int = 0
+
   def get_score(self):
     return self.score
 
@@ -15,7 +14,9 @@ class Player:
 class Game:
   player_1: Player = Player()
   player_2: Player = Player()
+
   def increment_score(self, player: Player):
+    print('CALLED INCREMENT SCORE')
     player.score += 15
 
 
@@ -27,10 +28,20 @@ def test_game_starts_at_zero_zero():
   assert empty_game.player_2.get_score() == 0, "Player 2 score isn't zero"
 
 
-def test_first_point():
+def test_player_1_scores_first_point():
   game = Game()
   game.increment_score(game.player_1)
   assert game.player_1.get_score() == 15, "Player 1 score isn't 15"
   assert game.player_2.get_score() == 0, "Player 2 score isn't zero"
 
+
+def test_player_1_scores_second_point():
+  game2 = Game()
+  # print(game2.player_1.get_score())
+  game2.increment_score(game2.player_1)
+  # print(game2.player_1.get_score())
+  game2.increment_score(game2.player_1)
+  # print(game2.player_1.get_score())
+  assert game2.player_1.get_score() == 30, "Player 1 score isn't 30"
+  assert game2.player_2.get_score() == 0, "Player 2 score isn't zero"
 
