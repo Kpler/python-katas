b78c618172d2869e17063056b3a80dcd83467274
khoa-trananh <anh_khoa.tran@outlook.com>
2022-12-08 10:48:42 +0100

    feat: add 2nd test

--- src/tennis_game/tennis_tdd_test.py
+++ src/tennis_game/tennis_tdd_test.py
@@ -6,14 +6,22 @@ from dataclasses import dataclass
 class Game():
   player_1_score: int = 0
   player_2_score: int = 0
+  def increment_player_1_score(self):
+    self.player_1_score += 15
 
 
 # tests here
 def test_game_starts_at_zero_zero():
-
   empty_game = Game()
 
   assert empty_game.player_1_score == 0, "Player 1 score isn't zero"
   assert empty_game.player_2_score == 0, "Player 2 score isn't zero"
+
+
+def test_first_point():
+  game = Game()
+  game.increment_player_1_score()
+  assert game.player_1_score == 15, "Player 1 score isn't 15"
+  assert game.player_2_score == 0, "Player 2 score isn't zero"
 
 
