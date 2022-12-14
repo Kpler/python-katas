6a7c701f0af2790825f470efdf81ac4d98b1c293
Petar Todorov <petre@314c.com>
2022-12-14 10:26:32 +0100

    first failing test

--- tests/diamond/test_diamond.py
+++ tests/diamond/test_diamond.py
@@ -1,5 +1,11 @@
-from src.diamond import make_diamond
+from diamond.diamond import make_diamond
+
 
 def test_print_diamond_with_a():
     expected = "A"
     assert(make_diamond("A")==expected)
+
+
+def test_print_diamond_with_ab():
+    expected = " A \nB B\n A "
+    assert make_diamond("B") == expected
