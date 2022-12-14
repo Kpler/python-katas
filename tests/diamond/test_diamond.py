55b3637243abdc225667e47d18f2f25a09f4898e
Nicolas Guignard <nicolas065@gmail.com>
2022-12-14 10:19:30 +0100

    test(diamond): add and make two elements diamond pass

--- tests/diamond/test_diamond.py
+++ tests/diamond/test_diamond.py
@@ -1,11 +1,20 @@
+from typing import Sequence
+
 import pytest as pytest
 
 
-def craft_diamond(widest_letter: str):
-    
-    pass
+def craft_diamond(widest_letter: str) -> Sequence[str]:
+    if widest_letter == 'A':
+        return [widest_letter]
+    else:
+        return ['A', f"{widest_letter}{widest_letter}", 'A']
 
 
 def test_single_element_diamond():
     diamond = craft_diamond('A')
-    assert diamond == 'A'
+    assert diamond == ['A']
+
+
+def test_two_elements_diamond():
+    diamond = craft_diamond('B')
+    assert diamond == ['A', 'BB', 'A']
