8d420be2a83a9bdcf99c17085af33dcc577d363b
Pierre Karpov <karpovpierre@gmail.com>
2022-12-14 17:31:40 +0800

    algo implementation

--- src/diamond/__init__.py
+++ src/diamond/__init__.py
@@ -12,16 +12,26 @@ def get_diamond(letter: str) -> str:
     rows = []
     for i in range(input_letter_index + 1):
         letter_this_row = alphabet[i]
-        number_of_spaces_edge = input_letter_index - 1 - i
+        number_of_spaces_edge = input_letter_index - i
         number_of_spaces_in_the_middle = size - 2 - number_of_spaces_edge * 2
-        row = (
-            SPACE * number_of_spaces_edge
-            + letter_this_row
-            + SPACE * number_of_spaces_in_the_middle
-            + letter_this_row
-            + SPACE * number_of_spaces_edge
-        )
+
+        if i == 0:
+            row = (
+                SPACE * number_of_spaces_edge
+                + letter_this_row
+                + SPACE * number_of_spaces_edge
+            )
+        else:
+            row = (
+                SPACE * number_of_spaces_edge
+                + letter_this_row
+                + SPACE * number_of_spaces_in_the_middle
+                + letter_this_row
+                + SPACE * number_of_spaces_edge
+            )
 
         rows.append(row)
+
+    rows = rows + list(reversed(rows[:-1]))
 
     return LINE_SEPARATOR.join(rows) + LINE_SEPARATOR
--- tests/diamond/test_diamond.py
+++ tests/diamond/test_diamond.py
@@ -9,3 +9,8 @@ def test_diamond_printer_a():
 def test_diamond_printer_b():
     result = get_diamond('B')
     assert result == ' A \nB B\n A \n'
+
+
+def test_diamond_printer_C():
+    result = get_diamond('C')
+    assert result == '  A  \n B B \nC   C\n B B \n  A  \n'
