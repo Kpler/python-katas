10cba6216532a1e9cc799c78e2704ce5246ca948
Gary <me@garyfibiger.cz>
2022-12-14 10:50:47 +0100

    feat(diamond): separation of concepts

--- src/diamond/__init__.py
+++ src/diamond/__init__.py
@@ -3,29 +3,51 @@ alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 LINE_SEPARATOR = "\n"
 SPACE = " "
+EMPTY_STRING = ""
 
 
-def get_diamond(letter: str) -> str:
+Diamond = list[str]
+
+
+def get_top_rows(letter: str) -> Diamond:
+    top_rows: Diamond = []
+
     input_letter_index = alphabet.index(letter)
-
     row_length = input_letter_index * 2 + 1
-    rows = []
+
     for i in range(input_letter_index + 1):
         letter_this_row = alphabet[i]
         number_of_spaces_edge = input_letter_index - i
         number_of_spaces_in_the_middle = row_length - 2 - number_of_spaces_edge * 2
 
         has_middle_spaces = number_of_spaces_in_the_middle > 0
+        second_letter = letter_this_row if has_middle_spaces else EMPTY_STRING
+
         row = (
             SPACE * number_of_spaces_edge
             + letter_this_row
             + SPACE * number_of_spaces_in_the_middle
-            + (letter_this_row if has_middle_spaces else "")
+            + second_letter
             + SPACE * number_of_spaces_edge
         )
 
-        rows.append(row)
+        top_rows.append(row)
 
-    rows = rows + list(reversed(rows[:-1]))
+    return top_rows
 
-    return LINE_SEPARATOR.join(rows) + LINE_SEPARATOR
+
+def get_rows(letter: str) -> Diamond:
+    top_rows = get_top_rows(letter)
+    bottom_rows = list(reversed(top_rows[:-1]))
+    all_rows = top_rows + bottom_rows
+    return all_rows
+
+
+def diamond_to_string(diamond: Diamond) -> str:
+    return LINE_SEPARATOR.join(diamond) + LINE_SEPARATOR
+
+
+def get_diamond(letter: str) -> str:
+    diamond = get_rows(letter)
+    output_string = diamond_to_string(diamond)
+    return output_string
--- tests/diamond/test_diamond.py
+++ tests/diamond/test_diamond.py
@@ -2,15 +2,15 @@ from src.diamond import get_diamond
 
 
 def test_diamond_printer_a():
-    result = get_diamond('A')
-    assert result == 'A\n'
+    result = get_diamond("A")
+    assert result == "A\n"
 
 
 def test_diamond_printer_b():
-    result = get_diamond('B')
-    assert result == ' A \nB B\n A \n'
+    result = get_diamond("B")
+    assert result == " A \nB B\n A \n"
 
 
 def test_diamond_printer_c():
-    result = get_diamond('C')
-    assert result == '  A  \n B B \nC   C\n B B \n  A  \n'
+    result = get_diamond("C")
+    assert result == "  A  \n B B \nC   C\n B B \n  A  \n"
