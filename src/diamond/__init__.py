54fff3ba41c87c8046358953411b5e1bf8f83bad
Grégoire Chauvet <gchauvet.home@gmail.com>
2022-12-14 10:14:26 +0100

    Beginning implementation

--- src/diamond/__init__.py
+++ src/diamond/__init__.py
@@ -1,3 +1,15 @@
-def get_diamond(letter: str):
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    return letter
+
+def get_diamond(letter: str) -> str:
+    input_letter_index = alphabet.index(letter)
+
+    size = input_letter_index * 2 + 1
+    rows = ""
+    for i in range(size):
+        number_of_spaces = input_letter_index - 1 - i
+        rows += " " * number_of_spaces + alphabet[i] + " " * number_of_spaces + "\n"
+
+        # rows += " " * size + "\n"
+
+    return rows
--- tests/diamond/test_diamond.py
+++ tests/diamond/test_diamond.py
@@ -3,9 +3,9 @@ from src.diamond import get_diamond
 
 def test_diamond_printer_a():
     result = get_diamond('A')
-    assert result == 'A'
+    assert result == 'A\n'
 
 
 def test_diamond_printer_b():
     result = get_diamond('B')
-    assert result == ' A \nB B\n A '
+    assert result == ' A \nB B\n A \n'
