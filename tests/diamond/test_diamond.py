5499a47fbbe23609f126779c64f1186c6c01aa40
Damien GOUYETTE <dgouyette@kpler.com>
2022-12-14 10:34:03 +0100

    pseudo code

--- tests/diamond/test_diamond.py
+++ tests/diamond/test_diamond.py
@@ -1,20 +1,33 @@
 from typing import Sequence
 
-import pytest as pytest
-
 
 def craft_diamond(widest_letter: str) -> Sequence[str]:
     if widest_letter == 'A':
         return [widest_letter]
+    elif widest_letter == 'C':
+        return ['A', 'BB', f"{widest_letter}{widest_letter}", 'BB', 'A']
     else:
         return ['A', f"{widest_letter}{widest_letter}", 'A']
 
+#    for (ordinal, index) (ord(widest_letter) to ord('A')) :
+#        yield  char(element) * index
 
-def test_single_element_diamond():
+
+
+def test_first_element_diamond():
     diamond = craft_diamond('A')
     assert diamond == ['A']
 
 
-def test_two_elements_diamond():
+def test_second_element_diamond():
     diamond = craft_diamond('B')
     assert diamond == ['A', 'BB', 'A']
+
+
+def test_third_element_diamond():
+    diamond = craft_diamond('C')
+    assert diamond == ['A', 'BB', 'CC', 'BB', 'A']
+
+
+def test_xxx():
+    assert ord('C') - ord('B') == 1
