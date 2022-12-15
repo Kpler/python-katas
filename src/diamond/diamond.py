0fd6cc1b00419093f6f00ac04c8ddc1200df9d99
ejaoua <ejaoua@kpler.com>
2022-12-15 18:02:24 +0100

    chore: add recursive

--- src/diamond/diamond.py
+++ src/diamond/diamond.py
@@ -7,8 +7,12 @@ def print_diamond(input: str) -> None:
     number_of_spaces = letter_ascii_number - A_ASCII_NUMBER
     print(build_diamond(input))
 
-def build_diamond(input: str) -> str:
-    return input
+def build_diamond(input: str, number_of_spaces: int = 0) -> str:
+    letter_ascii_number = ord(input)
+    distance_to_a = letter_ascii_number - A_ASCII_NUMBER
+    if distance_to_a == 0:
+        return " " + input + " "
+    return build_diamond(chr(letter_ascii_number - 1), distance_to_a) + '' + build_diamond(chr(letter_ascii_number - 1), distance_to_a)
 
 ''''
   A
