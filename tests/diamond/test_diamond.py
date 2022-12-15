4b618b2705bbe5ae2d1f7dfe4f6f6179fe65943e
ejaoua <ejaoua@kpler.com>
2022-12-15 17:17:16 +0100

    add print_diamond

--- tests/diamond/test_diamond.py
+++ tests/diamond/test_diamond.py
@@ -1,10 +1,18 @@
+import io
 import unittest
+from contextlib import redirect_stdout
+
+from src.diamond.diamond import print_diamond
+
 
 class TestDiamond(unittest.TestCase):
 
     def test_print(self):
         input = 'A'
         result = print_diamond(input)
-
+        f = io.StringIO()
+        with redirect_stdout(f):
+            print_diamond(input)
+        redirected_stdout = f.getvalue()
 
         self.assertEqual('A', result)
