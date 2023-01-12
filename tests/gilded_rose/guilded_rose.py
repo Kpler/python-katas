3a4fb5849721703521fb3584bd1cf5f4fc6ca608
Guillaume MOCQUET <gmocquet@kpler.com>
2023-01-12 17:28:43 +0100

    chore: add

--- tests/gilded_rose/guilded_rose.py
+++ tests/gilded_rose/guilded_rose.py
@@ -1,10 +1,21 @@
 import unittest
+from approvaltests.combination_approvals import verify, verify_all_combinations
 
 
-def verify(param):
-    pass
+# def verify(param):
+#     pass
+def dumy(input: int, multiply: int):
+    return input * multiply
 
 
 class GettingStartedTest(unittest.TestCase):
     def test_simple(self):
-        verify("Hello ApprovalTests")
+        # verify("Hello ApprovalTests")
+        # verify(dumy(2))
+
+        verify_all_combinations(
+            dumy, [
+                [2, 3, 4],
+                [1, 2, 3]
+            ]
+        )
