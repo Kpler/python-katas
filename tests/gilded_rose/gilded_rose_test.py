ee69f84583413eb7a087db460bd8fa74320827f4
Raphaël Carmona <rcarmona@kpler.com>
2023-01-12 17:32:26 +0100

    next steps

--- tests/gilded_rose/gilded_rose_test.py
+++ tests/gilded_rose/gilded_rose_test.py
@@ -1,15 +1,15 @@
 from unittest import TestCase
 
 import pytest
-from src.gilded_rose.guilded_rose import Item
+from src.gilded_rose.guilded_rose import Item, GildedRose
 import unittest
-from approvaltests import verify
+from approvaltests import verify, verify_all_combinations
 
 
 class TestGildedRose(unittest.TestCase):
   def test_gilded_rose(self):
     i = Item('name', 0, 0)
-    self.assertEquals(i.name, 'something')
+    self.assertEquals(i.name, 'name')
 
   def test_approval_gilded_rose(self):
     i = Item('name', 0, 0)
@@ -18,4 +18,9 @@ class TestGildedRose(unittest.TestCase):
   def test_update_quality(self):
     i = Item('test_1', 0, 0)
     j = Item('test_2', 1, 1)
+    gr = GildedRose([i, j])
+    self.assertEquals(gr.items[0].name, "test_1")
+    self.assertEquals(gr.items[1].name, "test_2")
 
+  def test_combo_guilded_rose(self):
+    ...
