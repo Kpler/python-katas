353a2b01aa863494abff34d54e7c5c05692a2010
mmetan0 <mmetan@kpler.com>
2023-01-13 17:22:43 +0800

    chore(gilded): mehmet

--- tests/gilded_rose/test_gilded_rose.py
+++ tests/gilded_rose/test_gilded_rose.py
@@ -4,7 +4,7 @@ from src.gilded_rose.guilded_rose import Item, GildedRose
 
 
 def test_update_quality():
-    item = Item("TestItem1", 0, 0)
+    item = Item("Aged Brie", 0, 0)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
     verify(item)
