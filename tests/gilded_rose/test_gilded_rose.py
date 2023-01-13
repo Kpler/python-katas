00a5fc90535f009a353acadfe4605bedc0f9cf4b
szeswee <song@kpler.com>
2023-01-13 17:42:47 +0800

    szeswee

--- tests/gilded_rose/test_gilded_rose.py
+++ tests/gilded_rose/test_gilded_rose.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from approvaltests.combination_approvals import verify_all_combinations
 
 from src.gilded_rose.guilded_rose import Item, GildedRose
@@ -7,11 +9,16 @@ def fn_1(var_1: int, var_2: int) -> int:
     return 9091 * var_1 * var_2
 
 
-def _execute_gilded_rose_update_quality() -> list[Item]:
-    gilded_rose = GildedRose([])
+def _execute_gilded_rose_update_quality(
+    names: list[Any], sell_ins: list[Any], qualities: list[Any]
+) -> list[Item]:
+    items = [
+        Item(name=name, sell_in=sell_in, quality=quality)
+        for name, sell_in, quality in zip(names, sell_ins, qualities)
+    ]
+    gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     return gilded_rose.items
-
 
 
 def test_simple():
@@ -19,8 +26,10 @@ def test_simple():
         fn_1, [list(range(20)), list(range(20))]
     )
 
+
 def test_gilded_rose():
-    verify_all_combinations(
-        _execute_gilded_rose_update_quality,
-        [],
-    )
+    names = ["foobar", "random", 123]
+    sell_ins = ["sell_in", 1, 888]
+    qualities = ["good", 1, 789]
+
+    verify_all_combinations(_execute_gilded_rose_update_quality, [names, sell_ins, qualities])
