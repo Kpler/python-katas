85fd89837b9f5ea45fea5d3eb97b897c9a682eeb
Gary <me@garyfibiger.cz>
2023-03-17 11:58:28 +0100

    chore(trip-service-3): test logged user

--- tests/trip_service/test_trip_service.py
+++ tests/trip_service/test_trip_service.py
@@ -6,21 +6,23 @@ from src.trip_service.trip_service import Trip, TripService, User
 def test_empty_trip_service() -> None:
     user = User()
     service = TripService()
-    with patch(
-        "src.trip_service.trip_service._get_logged_user", return_value=True
-    ):
+    with patch("src.trip_service.trip_service._get_logged_user", return_value=True):
         trips = service.get_trips_by_user(user)
         assert trips == []
 
 
-def test_single_trip_service() -> None:
-    user = User()
-    friend = User()
-    user.add_friend(friend)
+@patch("src.trip_service.trip_service._get_logged_user", return_value=True)
+@patch("src.trip_service.trip_service._find_trips_by_user")
+def test_single_trip_service(mocker_find_trips_by_user, mocked_get_logged_user) -> None:
+    logged_user = User()
+    mocked_get_logged_user.return_value = logged_user
+
+    query_user = User()
+    query_user.add_friend(logged_user)
+
     trip = Trip()
+    mocker_find_trips_by_user.return_value = [trip]
+
     service = TripService()
-    with patch(
-            "src.trip_service.trip_service._get_logged_user", return_value=True
-    ):
-        trips = service.get_trips_by_user(user)
-        assert trips == [trip]
+    trips = service.get_trips_by_user(query_user)
+    assert trips == [trip]
