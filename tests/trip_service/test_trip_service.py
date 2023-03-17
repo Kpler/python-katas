d994d9ca629c718ad47ba51dfab69fff7c0f5ae3
Damien GOUYETTE <dgouyette@kpler.com>
2023-03-17 12:06:35 +0100

    final code

--- tests/trip_service/test_trip_service.py
+++ tests/trip_service/test_trip_service.py
@@ -8,9 +8,17 @@ def user_with_no_friends():
     user = User()
     return user
 
+
 @pytest.fixture
-def user_with_one_friend():
+def user_with_one_friend(mocker):
     mock = mocker.patch("src.trip_service.trip_service._get_logged_user")
+
+
+@pytest.fixture
+def user_with_zero_friend(mocker):
+    mock = mocker.patch("src.trip_service.trip_service.user.get_friends")
+    mock.return_value = []
+    return mock
 
 
 def test_userservice_should_raise_exception_when_user_is_none(mocker):
@@ -22,15 +30,12 @@ def test_userservice_should_raise_exception_when_user_is_none(mocker):
         trip_service.get_trips_by_user(user)
 
 
-def test_trip_service_should_return_empty_list_when_user_has_no_friends(mocker):
-	user = User()
+def test_trip_service_should_return_empty_list_when_user_has_no_friends(mocker, user_with_zero_friend):
 
-	mock = mocker.patch("src.trip_service.trip_service._get_logged_user")
-	mock.return_value = user
+    mock = mocker.patch("src.trip_service.trip_service._get_logged_user")
+    mock.return_value = user_with_zero_friend
 
-	trip_service = TripService()
-	trips = trip_service.get_trips_by_user(user)
+    trip_service = TripService()
+    trips = trip_service.get_trips_by_user(user_with_zero_friend)
 
-	assert len(trips) == 0
-        
-
+    assert len(trips) == 0
