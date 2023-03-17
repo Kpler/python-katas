5a8f18f889de0e0a9ff61b7327ecba4a5c611f44
Damien GOUYETTE <dgouyette@kpler.com>
2023-03-17 11:34:38 +0100

    chore(room1): first working test

--- tests/trip_service/test_trip_service.py
+++ tests/trip_service/test_trip_service.py
@@ -1,13 +1,17 @@
-
 import pytest
 from src.trip_service.exceptions import UserNotLoggedInException
-from trip_service import TripService
+from src.trip_service.trip_service import TripService, User
+
 
 @pytest.fixture
 def user():
     pass
 
-def test_user_should_not_be_empty():
+
+def test_userservice_should_raise_exception_when_user_is_none(mocker):
+    mock = mocker.patch("src.trip_service.trip_service._get_logged_user")
+    user = None
+    mock.return_value = user
     trip_service = TripService()
-    trip_service.get_trips_by_user()
-    pytest.assertRaises(UserNotLoggedInException)
+    with pytest.raises(UserNotLoggedInException):
+        trip_service.get_trips_by_user(user)
