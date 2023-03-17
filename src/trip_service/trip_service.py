1a380ca0e5bb853fc5e7de78505b10f66f69e984
saad <sbalbiyad@kpler.com>
2023-03-17 12:05:47 +0100

    Refactoring 4

--- src/trip_service/trip_service.py
+++ src/trip_service/trip_service.py
@@ -17,11 +17,16 @@ class User:
     friends: list = dataclasses.field(default_factory=list)
     trips: list = dataclasses.field(default_factory=list)
 
+
     def add_friend(self, user: "User"):
         self.friends.append(user)
 
     def add_trip(self, trip: Trip):
         self.trips.append(trip)
+    
+    def has_friends(self, friend: "User") -> bool:
+        return friend in self.friends
+    
 
 
 class TripService:
@@ -30,9 +35,10 @@ class TripService:
         logged_user = _get_logged_user()
         if not logged_user:
             raise UserNotLoggedInException()
-
+        has_acess_friend = False
         is_friend = False
         trip_list = []
+
 
         for friend in user.friends:
             if friend is logged_user:
