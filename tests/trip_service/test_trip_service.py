59dd0f99f04ffab381f8331a8369391bb66f80a8
Nandane Djanarthany <ndjanarthany@kpler.com>
2023-03-17 11:32:29 +0100

    init first test

--- tests/trip_service/test_trip_service.py
+++ tests/trip_service/test_trip_service.py
@@ -1,5 +1,13 @@
 import pytest
 
-def test_get_trip_by_user:
+from unittest.mock import MagicMock
+
+from src.trip_service.trip_service import TripService
+
+def test_get_trip_by_user_raising():
+    ts = TripService();
+
+    with pytest.raises(DependantClassCallDuringUnitTestException):
+        ts.get_trips_by_user()
     
 
