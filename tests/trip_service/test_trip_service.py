c70839afdc2ae5bb11e00b86296bdda6e370cc2b
Bram Vandendriessche <50384080+Bramvd3@users.noreply.github.com>
2023-03-17 11:38:23 +0100

    refactoring

--- tests/trip_service/test_trip_service.py
+++ tests/trip_service/test_trip_service.py
@@ -1,13 +1,15 @@
 import pytest
-
+from src.trip_service.exceptions import DependantClassCallDuringUnitTestException, UserNotLoggedInException
 from unittest.mock import MagicMock
+from src.trip_service.trip_service import User
 
 from src.trip_service.trip_service import TripService
 
-def test_get_trip_by_user_raising():
-    ts = TripService();
+def test_raising_exception_when_user_is_not_logged():
+    ts = TripService()
+    not_logged_user = User()
 
     with pytest.raises(DependantClassCallDuringUnitTestException):
-        ts.get_trips_by_user()
+        ts.get_trips_by_user(not_logged_user)
     
 
