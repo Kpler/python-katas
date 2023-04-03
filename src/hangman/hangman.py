f44b203974c59d05c5cf6d1ff89382ebba9bdf47
Timothée Maurin <tmaurin@kpler.com>
2023-04-03 16:03:20 +0200

    Adding error test + win test

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -2,11 +2,18 @@ class Hangman:
     def __init__(self, word):
         self.word = word
         self.status = "ongoing"
-        self.current_guesses = ''.join(['_' * len(word)])
-        self.wrong_guesses = []
+        # self.current_guesses = ''.join(['_' * len(word)])
+        self.right_guesses = ""
+        self.wrong_guesses = ""
 
     def guess(self, letter):
-        # TODO
-        guessed_word = self.current_guesses
-        return guessed_word
+        if letter in self.word:
+            self.right_guesses += letter
+        else:
+            self.wrong_guesses += letter
+
+        current_result = "".join([l if l in self.right_guesses else "_" for l in self.word])
+        if self.wrong_guesses:
+            current_result += f" # {self.wrong_guesses}"
+        return current_result
 
--- tests/hangman/test_hangman.py
+++ tests/hangman/test_hangman.py
@@ -23,3 +23,24 @@ def test_guess():
     test_guess = hangman.guess(letter="a")
     assert test_guess == "_a___a_"
     assert hangman.status == "ongoing"
+
+
+def test_guess_wrong():
+    hangman = Hangman(
+        word="hangman"
+    )
+    test_guess = hangman.guess(letter="w")
+    assert test_guess == "_______ # w"
+    assert hangman.status == "ongoing"
+
+
+def test_guess_wrong():
+    hangman = Hangman(
+        word="hangman"
+    )
+    test_guess = hangman.guess(letter="h")
+    test_guess = hangman.guess(letter="a")
+    test_guess = hangman.guess(letter="n")
+    test_guess = hangman.guess(letter="g")
+    test_guess = hangman.guess(letter="m")
+    assert test_guess == f"You found the word! (\"hangman\")"
