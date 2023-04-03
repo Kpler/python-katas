37fda5f386d139d11de2fd3a06ef8591be36a9f9
Henry Nomichith <hnomichith@kpler.com>
2023-04-03 16:06:33 +0200

    henry

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -2,9 +2,14 @@ class Hangman:
     MISTAKES_ALLOWED = 6
 
     def __init__(self, word: str):
-        self.word = word
-        self.mistakes_left = self.MISTAKES_ALLOWED
+        self._word = word
+        self._mistakes_left = self.MISTAKES_ALLOWED
 
     def guess(self, letter: str) -> str:
-
-        return 'k___'
+        if letter in self._word:
+            return "k___"
+        else:
+            self._mistakes_left -= 1
+            if self._mistakes_left <= 0:
+                return "The game has ended."
+            return '____ # z'
--- tests/hangman/test_hangman.py
+++ tests/hangman/test_hangman.py
@@ -1,22 +1,27 @@
 from src.hangman.hangman import Hangman
 
 
-def test_dummy():
-    assert True
-
 def test_guess_found():
     game = Hangman('kata')
     curr = game.guess('k')
-
     assert curr == 'k___'
+
 
 def test_guess_not_found():
     game = Hangman('kata')
     curr = game.guess('z')
-
     assert curr == '____ # z'
-    assert game.mistakes_left == 5
+    assert game._mistakes_left == 5
+
 
 def test_mistakes():
     game = Hangman('')
-    assert Hangman.MISTAKES_ALLOWED == game.mistakes_left
+    assert Hangman.MISTAKES_ALLOWED == game._mistakes_left
+
+
+def test_too_much_mistakes():
+    game = Hangman('kata')
+    curr = ""
+    for _ in range(6):
+        curr = game.guess('z')
+    assert curr == "The game has ended."
