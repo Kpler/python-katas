7c3d81c8092296a558b5c8318d85205a30ff8890
pguardati-kpler <98783908+pguardati-kpler@users.noreply.github.com>
2023-04-03 15:35:17 +0200

    fix: add approval file

---
+++ src/hangman/hangman.py
@@ -0,0 +1,9 @@
+class Hangman:
+    def __init__(self, word):
+        self.word = word
+        self.status = "ongoing"
+
+    def guess(self, letter):
+        # TODO
+        pass
+
--- tests/hangman/test_hangman.py
+++ tests/hangman/test_hangman.py
@@ -1,2 +1,25 @@
-def test_dummy():
-    assert True
+from src.hangman.hangman import Hangman
+
+"""
+TODO: implement test for state machine
+ongoing
+loss
+victory
+ended
+"""
+
+
+def test_initialisation():
+    hangman = Hangman(
+        word="hangman",
+    )
+    assert hangman.word == "hangman"
+
+
+def test_guess():
+    hangman = Hangman(
+        word="hangman"
+    )
+    hangman.guess(letter="a")
+    assert hangman.word == "__a__a_"
+    assert hangman.status == "ongoing"
