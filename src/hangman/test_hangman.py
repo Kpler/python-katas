62743c622ac8f498bcd805ad9d51201ab26a33b3
Nicolas Guignard <nicolas065@gmail.com>
2023-04-03 15:47:24 +0200

    test: add two tests

--- src/hangman/test_hangman.py
+++ src/hangman/test_hangman.py
@@ -1,8 +1,21 @@
-from hangman import Hangman
 import pytest
+
+
+class Hangman:
+
+    def __init__(self, word):
+        pass
+
+    def guess(self, letter):
+        return f'_ _ _ _ # {letter}'
 
 
 def test_fails_on_letter_not_present_and_displays_the_letter():
     hangman = Hangman("word")
     result = hangman.guess("z")
     assert result == "_ _ _ _ # z"
+
+def test_fails_on_another_missing_letter():
+    hangman = Hangman("word")
+    result = hangman.guess("t")
+    assert result == "_ _ _ _ # t"
