ac2628529dc8adc40dd8e00b66a7e94775148164
Alex Gindre <alexgindre@gmail.com>
2023-04-21 11:40:04 +0200

    alex

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,11 +1,16 @@
 from typing import Tuple
+from enum import Enum   
 
+Operator = Enum('Operator', ['+','-','*','/'])
 
 def calculate(expression: str) -> float:
-    return sum(find_operands(expression))
+    operator = find_operator(expression)
+    return sum(find_operands(expression, operator))
 
-def find_operands(string: str) -> Tuple[int,int]:
-    return list(map(int, string.split("+")))
+def find_operands(string:str, operator: Operator) -> list[int,int]:
+    return list(map(int, string.split(operator.value)))
 
-def find_operator(string: str) -> str:
-    pass
+def find_operator(string: str) -> Operator:
+    for operator in Operator:
+        if operator.value in string:
+            return operator
--- tests/calculator/test_calculator.py
+++ tests/calculator/test_calculator.py
@@ -3,3 +3,8 @@ from src.calculator.calculator import calculate
 
 def test_calculator_add():
     assert calculate("1 + 1") == 2
+    assert calculate("1 + 1 + 1") == 3
+    assert calculate("2 + 1") == 3
+
+def test_calculator_substraction():
+    assert calculate("1 - 1") == 0
