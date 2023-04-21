cdeba31b40196829b0a9fcc9cca1b35786a47811
Namsam <snaim@kpler.com>
2023-04-21 11:34:18 +0200

    sam

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,7 +1,11 @@
 def calculate(expression: str) -> float:
     if not expression:
         return 0
-    if len(expression) == 1:
-        return float(expression)
-    return float(expression[0]) + float(expression[-1])
+    return get_first_number(expression) + calculate(expression[4:])
+
+
+def get_first_number(expression: str) -> float:
+    operator = "+"
+    print(float(expression.replace(" ", "").split(operator)))
+    return float(expression.replace(" ", "").split(operator)[0])
 
--- tests/calculator/test_calculator.py
+++ tests/calculator/test_calculator.py
@@ -4,14 +4,22 @@ from src.calculator.calculator import calculate
 def test_calculator_add():
     assert calculate("1 + 1") == 2
 
+
 def test_calculator_add_equals_3():
     assert calculate("1 + 2") == 3
+
 
 def test_calculator_with_empty_expression():
     assert calculate("") == 0
 
+
 def test_calculator_with_one_expression():
     assert calculate("1") == 1
 
+
 def test_addition_3_elements():
-    pass
+    assert calculate("1 + 1 + 1") == 3
+
+
+def test_complex_addition_3_elements():
+    assert calculate("10 + 20 + 5") == 35
