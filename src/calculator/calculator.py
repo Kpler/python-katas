876777b7c97800f7357f7b9ef4bf7ee03cc3b675
Pierre Karpov <karpovpierre@gmail.com>
2023-04-21 18:22:56 +0800

    multiplication wip

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,18 +1,28 @@
+
 def calculate(expression: str) -> float:
     if not expression:
         return 0
     first_number = get_first_number(expression)
-    next_expr_start_index = len(first_number) + 3
-    next_expr = expression[next_expr_start_index:]
+    next_expr = get_next_expression(expression, first_number)
+
     if not next_expr:
         return float(first_number)
-    operator = expression[len(first_number) + 1]
+    operator = get_next_operator(expression, first_number)
     if operator == "+":
         return float(first_number) + calculate(next_expr)
     elif operator == "-":
         return float(first_number) - calculate(next_expr)
+    elif operator == "*":
+        return float(first_number) * calculate(next_expr)
 
 
 def get_first_number(expression: str) -> str:
     return expression.split(" ")[0]
+
+def get_next_expression(initial_expr: str, first_number: str) -> str:
+    next_expr_start_index = len(first_number) + 3
+    return initial_expr[next_expr_start_index:]
+
+def get_next_operator(initial_expr: str, first_number: str) -> str:
+    return initial_expr[len(first_number) + 1]
 
--- tests/calculator/test_calculator.py
+++ tests/calculator/test_calculator.py
@@ -16,6 +16,8 @@ def test_calculator_with_empty_expression():
 def test_calculator_with_one_expression():
     assert calculate("1") == 1
 
+def test_calculator_with_one_negative_expression():
+    assert calculate("-1") == -1
 
 def test_addition_3_elements():
     assert calculate("1 + 1 + 1") == 3
@@ -31,3 +33,26 @@ def test_substract():
 
 def test_substract_equals_1():
     assert calculate("2 + -1") == 1
+
+def test_addition_then_substraction():
+    assert calculate("2 + 1 - 5") == -2
+
+def test_substract_negative_number():
+    assert calculate("2 - -1") == 3
+
+def test_multiplication():
+    assert calculate("2 * 5") == 10
+
+def test_multiplication_negative():
+    assert calculate("2 * -5") == -10
+
+
+def test_mulitple_multiplication():
+    assert calculate("-2 * -5 * 3") == 30
+
+
+
+def test_addition_then_multiplication_then_addition():
+        assert calculate("3 + 2 * 5") == 13
+def test_multiplication_then_addition():
+    assert calculate("2 * 5 + 3") == 13
