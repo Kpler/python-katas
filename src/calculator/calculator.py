5311c8aaf317efd66a4115ea740489707d37f9e0
Léo <ldureuil@kpler.com>
2023-04-21 17:54:56 +0800

    leo

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,32 +1,37 @@
 from operator import add, sub
 
 
+operations = {
+ "+": add,
+ "-": sub,
+}
+
 def calculate(expression: str) -> float:
     tokens = expression.split()
     head = tokens[0]
 
-    return rec(int(head), tokens)
+    if (len(tokens) % 2) == 0:
+        raise ValueError("No enough element")
+
+    return rec(int(head), tokens[1:])
 
 
 def rec(acc: int, tokens: list[str]) -> float:
     if len(tokens) == 0:
-        return 0
-    elif len(tokens) == 1:
-        return int(tokens[0])
+        return acc
     else:
         op = tokens[0]
         head = tokens[1]
 
-        if op == "+":
-            f = add
-        elif op == "-":
-            f = sub
-        else:
+        try:
+            operation = operations[op]
+        except:
             raise ValueError(f"Unexpected Operation {op}")
+       
         try:
             a = int(head)
 
         except:
             raise
 
-        return f(a, rec(tokens[2:]))
+        return rec(operation(acc, a), tokens[2:])
--- tests/calculator/test_calculator.py
+++ tests/calculator/test_calculator.py
@@ -1,26 +1,24 @@
 from src.calculator.calculator import calculate
+import pytest
+
+def test_calculator_single_number_0():
+     assert calculate("0") == 0
+
+def test_calculator_single_number_1():
+     assert calculate("1") == 1
 
 
-# def test_calculator_single_number_0():
-#     assert calculate("0") == 0
-#
-#
-# def test_calculator_single_number_1():
-#     assert calculate("1") == 1
-#
-#
-# def test_calculator_add():
-#     assert calculate("1 + 1") == 2
-#
-# def test_calculator_sub():
-#     assert calculate("3 - 2") == 1
+def test_calculator_add():
+     assert calculate("1 + 1") == 2
 
+def test_calculator_sub():
+     assert calculate("3 - 2") == 1
 
 def test_calculator_longer_add():
-    # assert calculate("4 + 8 + 15 + 16 + 23 + 42") == 108
+    assert calculate("4 + 8 + 15 + 16 + 23 + 42") == 108
     assert calculate("100 - 50 + 25") == 75
     assert calculate("10 + 20 - 5") == 25
 
-
-
-100 - 50 + 25
+def test_():
+    with pytest.raises(ValueError):
+        calculate("4 +")
