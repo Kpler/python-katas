fcbf5f68d3f9ae6f067c6e6507d0ee11ad4c509e
Gary <me@garyfibiger.cz>
2023-04-21 11:37:29 +0200

    feat(calculator): add longer tests

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,19 +1,25 @@
+from operator import add, sub
+
+
 def calculate(expression: str) -> float:
     tokens = expression.split()
 
     return rec(tokens)
 
+
 def rec(tokens):
     if len(tokens) == 0:
         return 0
+    elif len(tokens) == 1:
+        return int(tokens[0])
     else:
         head = tokens[0]
         op = tokens[1]
 
         if op == "+":
-            f = lambda x, y: x + y
+            f = add
         elif op == "-":
-            f = lambda x, y: x - y
+            f = sub
         else:
             raise ValueError(f"Unexpected Operation {op}")
         try:
@@ -22,4 +28,4 @@ def rec(tokens):
         except:
             raise
 
-        return a + rec(tokens[1:])
+        return f(a, rec(tokens[2:]))
