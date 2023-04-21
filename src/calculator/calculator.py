ae077180657a5ea2895be4b50b5d9f7a3c5a06d5
antoine charlet <acharlet@kpler.com>
2023-04-21 11:19:47 +0200

    antoine

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,2 +1,11 @@
+from typing import Tuple
+
+
 def calculate(expression: str) -> float:
     return 0
+
+def find_operands(string: str) -> Tuple[int,int]:
+    pass
+
+def find_operator(string: str) -> str:
+    pass
