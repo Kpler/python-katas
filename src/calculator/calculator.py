397543a9becbc9f377e858807536483f1f31e034
Vladislav Sokolov <vsokolov@kpler.com>
2023-04-21 11:54:52 +0200

    vlad 2

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -2,15 +2,17 @@ def calculate(expression: str) -> float:
     expr_no_space = remove_spaces(expression)
     if not expr_no_space:
         return 0
-    first_number, remainder_expr = get_first_number_and_expression(expr_no_space)
-    return first_number + calculate(remainder_expr)
+    first_number = get_first_number(expr_no_space)
+    next_expr_start_index = len(first_number) + 1
+    next_expr = expr_no_space[next_expr_start_index:]
+    return float(first_number) + calculate(next_expr)
 
 
 def remove_spaces(expression: str) -> str:
     return expression.replace(" ", "")
 
-def get_first_number_and_expression(expression: str) -> float:
+
+def get_first_number(expression: str) -> str:
     operator = "+"
-    split_values = expression.split(operator)
-    return float(split_values[0]), "+".join(split_values[1:])
+    return expression.split(operator)[0]
 
--- tests/calculator/test_calculator.py
+++ tests/calculator/test_calculator.py
@@ -14,7 +14,8 @@ def test_calculator_with_empty_expression():
 
 
 def test_calculator_with_one_expression():
-    assert calculate("10000+1") == 1
+    assert calculate("1") == 1
+
 
 def test_addition_3_elements():
     assert calculate("1 + 1 + 1") == 3
@@ -22,3 +23,7 @@ def test_addition_3_elements():
 
 def test_complex_addition_3_elements():
     assert calculate("10 + 20 + 5") == 35
+
+
+def test_substract():
+    assert calculate("2 - 1") == 1
