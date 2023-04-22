b0c84e134654055d6d7fd1df1a06d959d24ae6e0
Namsam <snaim@kpler.com>
2023-04-22 10:37:13 +0200

    sam level 1 + level 2

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,28 +1,61 @@
-
 def calculate(expression: str) -> float:
     if not expression:
         return 0
+
+    expr_without_multiplication = expression
+    if "*" in expr_without_multiplication:
+        expr_without_multiplication = manage_multiplication(expression)
+
+    return manage_addition_substraction(expr_without_multiplication)
+
+
+def manage_multiplication(expression: str):
+    split_expr = expression.split(" ")
+    expr_without_multiplication = []
+    for i in range(len(split_expr)):
+        if split_expr[i] == "*":
+            expr_without_multiplication[-1] = str(float(expr_without_multiplication[-1]) * float(split_expr[i + 1]))
+        elif split_expr[i - 1] == "*":
+            pass
+        else:
+            expr_without_multiplication.append(split_expr[i])
+
+    return " ".join(expr_without_multiplication)
+
+
+def manage_addition_substraction(expression: str) -> float:
     first_number = get_first_number(expression)
     next_expr = get_next_expression(expression, first_number)
-
     if not next_expr:
         return float(first_number)
     operator = get_next_operator(expression, first_number)
     if operator == "+":
         return float(first_number) + calculate(next_expr)
     elif operator == "-":
-        return float(first_number) - calculate(next_expr)
-    elif operator == "*":
-        return float(first_number) * calculate(next_expr)
+        return float(first_number) - calculate(make_opposite(next_expr))
 
 
 def get_first_number(expression: str) -> str:
     return expression.split(" ")[0]
 
+
 def get_next_expression(initial_expr: str, first_number: str) -> str:
     next_expr_start_index = len(first_number) + 3
     return initial_expr[next_expr_start_index:]
 
+
 def get_next_operator(initial_expr: str, first_number: str) -> str:
     return initial_expr[len(first_number) + 1]
 
+
+def make_opposite(expression: str) -> str:
+    split_expr = expression.split(" ")
+    opposite_expr = []
+    for value in split_expr:
+        if value == "+":
+            opposite_expr.append("-")
+        elif value == "-":
+            opposite_expr.append("+")
+        else:
+            opposite_expr.append(value)
+    return " ".join(opposite_expr)
--- tests/calculator/test_calculator.py
+++ tests/calculator/test_calculator.py
@@ -1,14 +1,6 @@
 from src.calculator.calculator import calculate
 
 
-def test_calculator_add():
-    assert calculate("1 + 1") == 2
-
-
-def test_calculator_add_equals_3():
-    assert calculate("1 + 2") == 3
-
-
 def test_calculator_with_empty_expression():
     assert calculate("") == 0
 
@@ -16,8 +8,18 @@ def test_calculator_with_empty_expression():
 def test_calculator_with_one_expression():
     assert calculate("1") == 1
 
+
 def test_calculator_with_one_negative_expression():
     assert calculate("-1") == -1
+
+
+def test_calculator_add():
+    assert calculate("1 + 1") == 2
+
+
+def test_calculator_add_equals_3():
+    assert calculate("1 + 2") == 3
+
 
 def test_addition_3_elements():
     assert calculate("1 + 1 + 1") == 3
@@ -34,14 +36,34 @@ def test_substract():
 def test_substract_equals_1():
     assert calculate("2 + -1") == 1
 
+
 def test_addition_then_substraction():
     assert calculate("2 + 1 - 5") == -2
+
+
+def test_substraction_then_addition():
+    assert calculate("2 - 5 + 1") == -2
+
+
+def test_substraction_then_addition_negative():
+    assert calculate("2 - -5 + -1") == 6
+
 
 def test_substract_negative_number():
     assert calculate("2 - -1") == 3
 
+
+def test_readme_level_1():
+    assert calculate("1 + 2") == 3
+    assert calculate("5 - 3") == 2
+    assert calculate("10 + 20 - 5") == 25
+    assert calculate("100 - 50 + 25") == 75
+    assert calculate("4 + 8 + 15 + 16 + 23 + 42") == 108
+
+
 def test_multiplication():
     assert calculate("2 * 5") == 10
+
 
 def test_multiplication_negative():
     assert calculate("2 * -5") == -10
@@ -51,8 +73,17 @@ def test_mulitple_multiplication():
     assert calculate("-2 * -5 * 3") == 30
 
 
-
 def test_addition_then_multiplication_then_addition():
-        assert calculate("3 + 2 * 5") == 13
+    assert calculate("3 + 2 * 5") == 13
+
+
 def test_multiplication_then_addition():
     assert calculate("2 * 5 + 3") == 13
+
+
+def test_readme_level_2():
+    assert calculate("2 * 3") == 6
+    assert calculate("2 * 5 * 10") == 100
+    assert calculate("2 + 3 * 4") == 14
+    assert calculate("2 * 3 + 4") == 10
+    assert calculate("3 * 4 + 5 * 6") == 42
