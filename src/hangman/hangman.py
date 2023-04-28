be2358ed55ba56222927eb353cf87811dc0a6f2f
Tangi Vass <tvass@kpler.com>
2023-04-28 15:39:31 +0200

    fix: tests

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -14,10 +14,10 @@ class Hangman:
                     self._current_word = self._current_word[:pos] + letter + self._current_word[pos+1:]
             if self._current_word == self._word:
                 return f"You found the word! ({self._word})"
-            return self._current_word
+            return " ".join(self._current_word)
 
         else:
             self._mistakes_left -= 1
             if self._mistakes_left <= 0:
                 return "The game has ended."
-            return self._current_word + ' # ' + letter
+            return " ".join(self._current_word) + ' # ' + letter
