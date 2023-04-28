4a34785c5953001a5a805163d6e4bbf723aed67f
davidlsq <contact@davidlsq.fr>
2023-04-28 15:40:07 +0200

    First commit

---
+++ src/hangman/hangman.py
@@ -0,0 +1,3 @@
+class Hangman:
+    def __init__(self, word):
+        self.word = word
--- tests/hangman/test_hangman.py
+++ tests/hangman/test_hangman.py
@@ -1,2 +1,5 @@
+from src.hangman.hangman import Hangman
+
 def test_dummy():
+    test = Hangman("word")
     assert True
