9151315730422641caf46f5c0a3743ffbfac17e3
Henry Nomichith <hnomichith@kpler.com>
2023-04-28 15:58:29 +0200

    henry's turn

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -3,11 +3,16 @@
 class Hangman:
     def __init__(self, result: str) -> None:
         self.result = result
-        self.prompt = "_" * len(self.result)
+        self.prompt = list("_" * len(self.result))
+        self.errors = set()
         
-    def guess(self, s: str) -> None:
+    def guess(self, s: str) -> str:
+
+        if s not in self.result:
+            self.errors.add(s)
+
         def c(i: int) -> bool:
-            return s in self.result
+            return s == self.result[i]
         l = list(range(len(self.result)))
         indexes = filter(c, l)
         for idx in indexes:
@@ -17,4 +22,6 @@ class Hangman:
             print(f"Success: {self.result}")
         else:
             print(self.prompt)
+
+        return ''.join(self.prompt) + ' # ' + ''.join(self.errors)
         
--- tests/hangman/test_hangman.py
+++ tests/hangman/test_hangman.py
@@ -1,17 +1,27 @@
-from hangman.hangman import Hangman
+from src.hangman.hangman import Hangman
+
 def test_hangman_success():
     h = Hangman("toto")
-    h.guess("o")
-    print(h.prompt)
-    assert h.prompt == "_o_o"
+    result = h.guess("o")
+    assert result == "_o_o"
     
 def test_hangman_fail():
     h = Hangman("toto")
+    result = h.guess("u")
+    assert result == "____ # u"
+
+def test_hangman_fail_twice():
+    h = Hangman("toto")
     h.guess("u")
-    print(h.prompt)
-    assert h.prompt == "____" 
-    
-    
+    result = h.guess("u")
+    assert result == "____ # u"
+
+def test_hangman_successfully_finished():
+    h = Hangman("toto")
+    h.guess("o")
+    result = h.guess("t")
+    assert result == "_o_o"
+
 if __name__ == "__main__":
     test_hangman_success()
 
