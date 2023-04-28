77e4664265b7c37c12ed52b2028e9bb95522442e
Tangi Vass <tvass@kpler.com>
2023-04-28 16:15:30 +0200

    feat: complete specs

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
+
+class GameStatus(Enum):
+    PLAYING = 1
+    WON = 2
+    LOST = 3
+
+
 class Hangman:
     MISTAKES_ALLOWED = 6
     HIDE_CHARACTER = '_'
@@ -6,18 +15,34 @@ class Hangman:
         self._word = word
         self._mistakes_left = self.MISTAKES_ALLOWED
         self._current_word = self.HIDE_CHARACTER*len(self._word)
+        self._failed_letters = ""
+        self.game_status = GameStatus.PLAYING
+
+    def _format_result(self) -> str:
+        part1 = " ".join(self._current_word)
+        part2 = " # " if self._failed_letters else ""
+        part3 = self._failed_letters
+
+        return part1 + part2 + part3
 
     def guess(self, letter: str) -> str:
+        if self.game_status != GameStatus.PLAYING:
+            return "The game has ended."
+        result = self._format_result()
+        if letter in result:
+            return result
         if letter in self._word:
             for pos, char in enumerate(self._word):
                 if char == letter:
                     self._current_word = self._current_word[:pos] + letter + self._current_word[pos+1:]
             if self._current_word == self._word:
+                self.game_status = GameStatus.WON
                 return f"You found the word! ({self._word})"
-            return " ".join(self._current_word)
-
         else:
             self._mistakes_left -= 1
             if self._mistakes_left <= 0:
-                return "The game has ended."
-            return " ".join(self._current_word) + ' # ' + letter
+                self.game_status = GameStatus.LOST
+                return f"You got hung! The word was {self._word}."
+            self._failed_letters += letter
+
+        return self._format_result()
--- src/hangman/test_hangman.py
+++ src/hangman/test_hangman.py
@@ -11,8 +11,9 @@ def test_fails_on_letter_not_present_and_displays_the_letter():
 
 def test_fails_on_another_missing_letter():
     hangman = Hangman("word")
+    result = hangman.guess("z")
     result = hangman.guess("t")
-    assert result == "_ _ _ _ # t"
+    assert result == "_ _ _ _ # zt"
 
 
 def test_successes_with_a_present_letter():
@@ -21,7 +22,63 @@ def test_successes_with_a_present_letter():
     assert result == "w _ _ _"
 
 
+def test_guess_same_letter_twice():
+    hangman = Hangman("word")
+    result = hangman.guess("w")
+    assert result == "w _ _ _"
+    result = hangman.guess("w")
+    assert result == "w _ _ _"
+    result = hangman.guess("z")
+    assert result == "w _ _ _ # z"
+    result = hangman.guess("z")
+    assert result == "w _ _ _ # z"
+
+
 def test_successes_with_a_second_present_letter():
     hangman = Hangman("word")
     hangman.guess("w")
     assert hangman.guess("o") == "w o _ _"
+
+def test_keep_first_failure():
+    word = "word"
+    hangman = Hangman(word)
+    hangman.guess("w")
+    hangman.guess("z")
+    result = hangman.guess("o")
+    assert result == "w o _ _ # z"
+
+
+def test_keep_all_failures():
+    word = "word"
+    hangman = Hangman(word)
+    hangman.guess("w")
+    hangman.guess("z")
+    result = hangman.guess("y")
+    assert result == "w _ _ _ # zy"
+
+
+def test_win_game():
+    word = "word"
+    hangman = Hangman(word)
+    hangman.guess("w")
+    hangman.guess("z")
+    result1 = hangman.guess("o")
+    assert result1 == "w o _ _ # z"
+    hangman.guess("r")
+    result2 = hangman.guess("d")
+    expected = f"You found the word! ({word})"
+    assert result2 == expected
+    result3 = hangman.guess("g")
+    assert result3 == "The game has ended."
+
+def test_lose_game():
+    word = "word"
+    hangman = Hangman(word)
+    hangman.guess("y")
+    hangman.guess("z")
+    hangman.guess("a")
+    hangman.guess("b")
+    hangman.guess("c")
+    hangman.guess("d")
+    result = hangman.guess("e")
+    assert result == f"You got hung! The word was {word}."
