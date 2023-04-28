61486d0ac93f7b5a3e134f5150f8b63934235c6e
JulienPiquot <jpiquot@kpler.com>
2023-04-28 16:27:18 +0200

    implement end of game logic

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -4,14 +4,29 @@ class Hangman:
         self.word = word
         self.game_state = set()
         self.number_of_mistakes = 0
+        self.game_ended = False
 
     def guess(self, letter: str) -> str:
+        if self.game_ended:
+            return "The game has ended."
+
         if letter not in self.word:
             self.number_of_mistakes = self.number_of_mistakes + 1
             if self.number_of_mistakes == 6:
+                self.game_ended = True
                 return "Game over"
+
         self.game_state.add(letter)
+        if self.__is_game_successful():
+            self.game_ended = True
+            return f"You found the word! ({self.word})"
         return self.__get_game_state()
+
+    def __is_game_successful(self) -> bool:
+        for letter in self.word:
+            if letter not in self.game_state:
+                return False
+        return True
 
     def __get_game_state(self) -> str:
         game_state_str = []
--- tests/hangman/test_hangman.py
+++ tests/hangman/test_hangman.py
@@ -10,11 +10,21 @@ def test_create_hangman():
     assert hangman.guess("r") == "w o r _"
 
 
+def test_successful_game():
+    hangman = Hangman("word")
+    assert hangman.guess("w") == "w _ _ _"
+    assert hangman.guess("o") == "w o _ _"
+    assert hangman.guess("r") == "w o r _"
+    assert hangman.guess("d") == "You found the word! (word)"
+    assert hangman.guess("d") == "The game has ended."
+
+
 def test_number_of_mistakes():
     hangman = Hangman("word")
     assert hangman.number_of_mistakes == 0
     hangman.guess("a")
     assert hangman.number_of_mistakes == 1
+
 
 def test_game_over():
     hangman = Hangman("word")
@@ -24,5 +34,6 @@ def test_game_over():
     hangman.guess("a")
     hangman.guess("a")
     assert hangman.guess("a") == "Game over"
+    assert hangman.guess("w") == "The game has ended."
 
 
