6bd954f0f19e0a6bb5d738b2ffbd8759219f37dc
Henry Nomichith <hnomichith@kpler.com>
2023-05-24 14:32:43 +0200

    henry

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,8 +1,13 @@
+operators: list[str] = ["+", "-"]
+
 def calculate(expression: str) -> float:
     split_expression = expression.split(" ")
 
+    unresolved_opertors: list[Operators]= []
     sum = 0
     for n in split_expression:
-        if n.isdigit():
+        if n in operators:
+            unresolved_opertors += n
+        elif n.isdigit():
             sum += float(n)
     return sum
