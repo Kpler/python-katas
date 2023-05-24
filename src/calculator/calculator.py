abc23d716c722627f076ab7b10938f4f7eb9e016
akulenovic97 <akulenovic97@gmail.com>
2023-05-24 08:40:57 -0400

    there is library that does addition for us

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,13 +1,28 @@
-operators: list[str] = ["+", "-"]
+from enum import Enum
+from operator import add, subtract
+class Operators(Enum):
+    ADD = "+"
+    SUBTRACT = "-"
+    MULTIPLY = "*"
+    DIVIDE = "/"
+
+    def map_to_operator(self):
+        match self:
+            case Operators.ADD:
+                return add
+            case Operators.SUBTRACT:
+                return subtract
 
 def calculate(expression: str) -> float:
     split_expression = expression.split(" ")
 
     unresolved_opertors: list[Operators]= []
+
     sum = 0
     for n in split_expression:
-        if n in operators:
+        if n in [o.value for o in Operators]:
             unresolved_opertors += n
         elif n.isdigit():
             sum += float(n)
+        print(unresolved_opertors)
     return sum
