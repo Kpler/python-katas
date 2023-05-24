f0b6615deca4729f66df54b245cac31064c38d79
akulenovic97 <akulenovic97@gmail.com>
2023-05-24 08:14:10 -0400

    i know what one plus plus is

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,2 +1,6 @@
 def calculate(expression: str) -> float:
-    return 0
+    split_expression = expression.split(" ")
+    if '+' in split_expression:
+        return float(split_expression[0]) + float(split_expression[2])
+    else:
+        return 0
--- tests/calculator/test_calculator.py
+++ tests/calculator/test_calculator.py
@@ -3,3 +3,6 @@ from src.calculator.calculator import calculate
 
 def test_calculator_add():
     assert calculate("1 + 1") == 2
+
+def test_calculator_addition_multiple():
+    assert calculate("1 + 2 + 3") == 6
