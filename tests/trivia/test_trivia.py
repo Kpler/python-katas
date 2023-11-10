511e35f43105b76c30cf1a470e611c9ed12240b1
Henry Nomichith <hnomichith@kpler.com>
2023-11-10 12:30:05 +0100

    our test are good

--- tests/trivia/test_trivia.py
+++
@@ -1,8 +0,0 @@
-import pytest as pytest
-
-an_example_to_use_parametrize = [42, 42, 42]
-
-
-@pytest.mark.parametrize("example", an_example_to_use_parametrize)
-def test_trivia(example):
-    assert 42 == example
--- tests/trivia/test_trivia_1.py
+++ tests/trivia/test_trivia_1.py
@@ -2,20 +2,24 @@ import pytest as pytest
 import io
 from contextlib import redirect_stdout
 from src.trivia.trivia import main
+from tests.trivia.create_golden_masters import DIRECTORY
 
-an_example_to_use_parametrize = [("test_1.txt", [1, ["Chet", "Pat", "Sue"]])]
+an_example_to_use_parametrize = [
+    ("test_1.txt", [1, ["Chet", "Pat", "Sue"]]),
+    ("test_2.txt", [3, ["Chet", "Pat", "Sue"]]),
+]
 
 
 @pytest.mark.parametrize("file_name, inputs", an_example_to_use_parametrize)
 def test_trivia_first_run(file_name, inputs):
-    assert file_name == "test_1.txt"
+    assert file_name == file_name
 
     f = io.StringIO()
     with redirect_stdout(f):
-        main(1, ["Chet", "Pat", "Sue"])
+        main(*inputs)
     redirected_stdout = f.getvalue()
 
-    with open(file_name, "r") as e:
+    with open(f"{DIRECTORY}/{file_name}", "r") as e:
         file_name_stdout = e.read()
 
     assert redirected_stdout == file_name_stdout
