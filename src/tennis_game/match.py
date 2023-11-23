913b168ed026d858dfbd87d4d71ab24bc05fa3d8
Nickolai Lychagin <nlychagin-ext@kpler.com>
2023-11-23 18:39:32 +0500

    Add tests score points

--- src/tennis_game/match.py
+++ src/tennis_game/match.py
@@ -2,3 +2,17 @@ class Match():
     def __init__(self) -> None:
         self.player1_score = "L"
         self.player2_score = "L"
+
+        self.player1_points = 0
+        self.player1_points = 0
+
+        self.point_dict = {
+            0: "L",
+            1: "15",
+            2: "30",
+            3: "40",
+        }
+
+
+    def game(self):
+        pass
--- tests/tennis_game/test_tennis_game.py
+++ tests/tennis_game/test_tennis_game.py
@@ -1,9 +1,19 @@
 from src.tennis_game.match import Match
 
-def test_assure_scores():
+import pytest
+
+
+@pytest.fixture
+def match():
+    return Match()
+
+
+def test_assure_scores(match):
     allowed_states = ["L", "15", "30", "40", "A"]
-    match = Match()
     assert match.player1_score in allowed_states
     assert match.player2_score in allowed_states
 
-def 
+def test_points_dict(match):
+    assert match.player1_score == "L"
+    match.player1_points += 1
+    assert match.player1_score == "15"
