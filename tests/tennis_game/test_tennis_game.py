c3b93587265455734ccb50c1c010621b46447f5a
Nikos Terlemes <nikos.terlemes@marinetraffic.com>
2023-11-23 14:24:22 +0100

    test_assure_scores

--- tests/tennis_game/test_tennis_game.py
+++ tests/tennis_game/test_tennis_game.py
@@ -1,2 +1,5 @@
+
 def test_assure_scores():
-    match.player1_score_
+    allowed_states = ["L", "15", "30", "40", "A"]
+    assert match.player1_score in allowed_states
+    assert match.player2_score in allowed_states
