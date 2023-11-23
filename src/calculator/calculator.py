3a3a1f5118ecd68ca46095f4ae82e3bed7784843
Thomas Biris <tbiris@marinetraffic.com>
2023-11-23 15:24:38 +0200

    feat(test): noob driver

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,4 +1,14 @@
 def calculate(expression: str) -> float:
-    return 0
 
-print ('test1')
+    try:
+        result = eval(expression)
+    except (SyntaxError, ZeroDivisionError) as e:
+        raise ValueError(f"Invalid expression: {expression}. Error: {e}")
+    
+    return result
+
+try:
+    result = calculate("5 + 3 * 2")
+    print("Result:", result)
+except ValueError as e:
+    print("Error:", e)
---
+++ tests/tennis_game/test_tennis.py
@@ -0,0 +1,8 @@
+from src.tennis_game.tennis import Game
+
+
+def test_tennis():
+    game=Game()
+    score=game.score()
+    assert True
+
