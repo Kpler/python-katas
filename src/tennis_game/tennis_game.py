35dba00f37392d47bd0bd519bc743d69af6c6e3e
Daniel Halmschlager <dhalmschlager@kpler.com>
2023-11-23 14:22:48 +0100

    Update

---
+++ src/tennis_game/tennis_game.py
@@ -0,0 +1,10 @@
+
+
+class TennisGame():
+
+    def __init__(self):
+        self.player1_score = 0
+        self.plater2_score = 0
+    
+    def score(self):
+        pass
--- tests/tennis_game/test_tennis.py
+++ tests/tennis_game/test_tennis.py
@@ -1,2 +1,11 @@
-def test_set() -> None:
-    pass
+from src.tennis_game.tennis_game import TennisGame
+
+
+def test_initial_state() -> None:
+    """Test that the initial state of the game is correct."""
+
+    game = TennisGame()
+    assert game.player1_score == 0
+    assert game.player2_score == 0
+
+    assert game.score() == "love-love"
