f4b7cea03d7f4750e379df3957a0d81d407965cd
Jason Mavromatakis <jmavromatakis@kpler.com>
2023-11-23 15:30:01 +0200

    Match class

---
+++ src/tennis_game/match.py
@@ -0,0 +1,4 @@
+class Match():
+    def __init__(self) -> None:
+        self.player1_score = "L"
+        self.player2_score = "L"
--- tests/tennis_game/test_tennis_game.py
+++ tests/tennis_game/test_tennis_game.py
@@ -1,5 +1,9 @@
+from src.tennis_game.match import Match
 
 def test_assure_scores():
     allowed_states = ["L", "15", "30", "40", "A"]
+    match = Match()
     assert match.player1_score in allowed_states
     assert match.player2_score in allowed_states
+
+def 
