764a9a38d8f11612090a46d01bd76abb7b5d3ee7
Nickolai Lychagin <nlychagin-ext@kpler.com>
2023-11-30 19:23:07 +0500

    add some tests

---
+++ src/tennis_game/game.py
@@ -0,0 +1,36 @@
+class Player:
+    def __init__(self, name: str):
+        self.points = 0
+        self.name = name
+
+class Game:
+
+    def __init__(self):
+        self.player1 = Player("player1")
+        self.player2 = Player("player2")
+
+        self.score_table = {
+            0: "love",
+            1: "15",
+            2: "30",
+            3: "40",
+        }
+
+    def get_score(self, player):
+        return self.score_table.get(player.points)
+
+    def get_winner(self):
+        if self.player1.points == 4 and (self.player1.points - self.player2.points) > 1:
+            return self.player1.name
+        elif self.player2.points == 4 and (self.player2.points - self.player1.points) > 1:
+            return self.player2.name
+        else:
+            return None
+
+    def add_points(self, player):
+        player.points += 1
+
+    def print_score(self):
+        if self.player1.points < 4 and self.player2.points < 4:
+            return f"{self.score_table.get(self.player1.points)}:{self.score_table.get(self.player2.points)}"
+
---
+++ tests/tennis_game/test_tennis.py
@@ -0,0 +1,61 @@
+from src.tennis_game.game import Game
+
+
+def test_if_score_in_allowed_values():
+    game = Game()
+    score_1 = game.get_score(game.player1)
+    score_2 = game.get_score(game.player2)
+    allowed_scores = ["love", "15", "30", "40", "adv"]
+    assert score_1 in allowed_scores
+    assert score_2 in allowed_scores
+
+def test_if_winning():
+    game = Game()
+    game.player1.points = 3
+    game.player2.points = 1
+
+    game.player1.points += 1
+
+    assert game.get_winner() == game.player1.name
+
+def test_if_deuce():
+    game = Game()
+    game.player1.points = 4
+    game.player2.points = 4
+    assert game.get_winner() is None
+
+
+def test_difference_less_than_one():
+    game = Game()
+    game.player1.points = 5
+    game.player2.points = 4
+    assert game.get_winner() is None
+
+
+def test_points_more_than_four():
+    game = Game()
+    game.player1.points = 5
+    game.player2.points = 5
+    assert game.get_winner() is None
+
+
+def test_add_score():
+    game = Game()
+    game.add_points(game.player1)
+    game.add_points(game.player1)
+    assert game.player1.points == 2
+
+
+def test_scores_below_four():
+    game = Game()
+    game.player1.points = 2
+    game.player2.points = 1
+    assert game.print_score() == "30:15"
+
+
+def test_scores_below_four():
+    game = Game()
+    game.player1.points = 2
+    game.player2.points = 1
+    assert game.print_score() == "30:15"
+
