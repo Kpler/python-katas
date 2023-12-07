8e11433a74b2bb9bdcfb2c0d50ee27223b9a13ad
Daniel Halmschlager <dhalmschlager@kpler.com>
2023-12-07 14:13:52 +0100

    feat: init game

---
+++ src/tennis_game/tennis.py
@@ -0,0 +1,8 @@
+
+
+class Game:
+    def __init__(self):
+        self.score = 'love-love'
+
+    def ball_result(self, player: int):
+        self.score = '15-love'
---
+++ tests/tennis_game/test_tennis.py
@@ -0,0 +1,10 @@
+from src.tennis_game.tennis import Game
+
+def test_init_game():
+    game = Game()
+    assert game.score == 'love-love'
+
+def test_player1_scoring():
+    game = Game()
+    game.ball_result(player=1)
+    assert game.score == '15-love'
