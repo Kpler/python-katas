3ba21ad723f0c3caef1c905d13de10092d3b96ff
Jason Mavromatakis <jmavromatakis@kpler.com>
2023-12-07 15:51:01 +0200

    j-commit

--- src/tennis_game/tennis.py
+++ src/tennis_game/tennis.py
@@ -1,7 +1,21 @@
 
+
 class Game():
+    player1_points = 0
+    player2_points = 0
     player1_score = "love"
     player2_score = "love"
-    
+    winner = None
+
     def __int__(self):
         pass
+
+    def get_winner(self):
+        self.winner = None
+        if self.player1_points == 4:
+            self.winner = "player1"
+        elif self.player2_points == 4:
+            self.winner = "player2"
+        else:
+            self.winner = None
+        return self.winner
--- tests/tennis_game/test_tennis.py
+++ tests/tennis_game/test_tennis.py
@@ -9,5 +9,27 @@ def test_score_system():
 
 def test_score_format():
     game = Game()
-    game.player1_point = 1
-    assert game.player1_score == "15"
+    game.player1_score = "love"
+    assert game.player1_score in possible_score
+    game.player1_score = "15"
+    assert game.player1_score in possible_score
+    game.player1_score = "30"
+    assert game.player1_score in possible_score
+    game.player1_score = "40"
+    assert game.player1_score in possible_score
+
+def test_win_game_player1():
+    game = Game()
+    game.player1_points = 3
+    game.player1_score = "40"
+    game.player1_points += 1
+    game.get_winner()
+    assert game.winner == "player1"
+
+def test_win_game_player2():
+    game = Game()
+    game.player2_points = 3
+    game.player2_score = "40"
+    game.player2_points += 1
+    game.get_winner()
+    assert game.winner == "player2"
