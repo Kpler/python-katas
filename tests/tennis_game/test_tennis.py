50080b3037d3d6bf518d040a9ed26eb6bb8f25be
Vladislav Sokolov <vsokolov@kpler.com>
2023-12-07 14:26:47 +0100

    vlad

--- tests/tennis_game/test_tennis.py
+++ tests/tennis_game/test_tennis.py
@@ -14,3 +14,23 @@ def test_player1_scoring():
     assert game.score == '30-love'
     game.ball_result(player=1)
     assert game.score == '40-love'
+
+
+def test_player2_scoring():
+    game = Game()
+    game.ball_result(player=2)
+    assert game.score == 'love-15'
+    game.ball_result(player=2)
+    assert game.score == 'love-30'
+    game.ball_result(player=2)
+    assert game.score == 'love-40'
+
+
+def test_win_player_1():
+    game = Game()
+    game.ball_result(player=1)
+    game.ball_result(player=1)
+    game.ball_result(player=1)
+    game.ball_result(player=1)
+
+    assert game.score == "Player 1 wins"
