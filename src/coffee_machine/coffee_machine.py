50a0cbb04a69faa01e82041abd5eb9c79bc977cf
Nickolai Lychagin <nlychagin-ext@kpler.com>
2023-12-14 18:35:08 +0500

    amend tests

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -1,6 +1,6 @@
 class CoffeeMachine:
 
-    def order(self, order_string: str) -> str:
+    def order(self, order_string: str | None) -> str:
         order_string.split(":")
         return "Drink maker makes 1 tea with no sugar - and therefore no stick"
 
--- tests/coffee_machine/test_coffee_machine.py
+++ tests/coffee_machine/test_coffee_machine.py
@@ -9,11 +9,9 @@ def test_init():
 
 def test_order_tea():
     coffee_machine = CoffeeMachine()
-    assert coffee_machine.order("T::") == ("Drink maker makes 1 tea with no sugar - "
-                                           "and therefore no stick")
+    assert coffee_machine.order() == "T::"
 
 
 def test_order_tea_with_sugar():
     coffee_machine = CoffeeMachine()
-    assert coffee_machine.order("T:1:0") == ("Drink maker makes 1 tea "
-                                             "with 1 sugar and a stick")
+    assert coffee_machine.order() == "T:1:0"
