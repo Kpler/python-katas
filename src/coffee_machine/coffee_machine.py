c268b2eb9c1717407da4fefc1992cd216ad29a6d
dkavu <dkavoulakos@kpler.com>
2023-12-14 15:29:22 +0200

    dani drink split

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -1,2 +1,6 @@
 class CoffeeMachine:
-    pass
+
+    def order(self, order_string: str) -> str:
+        order_string.split(":")
+        return "Drink maker makes 1 tea with no sugar - and therefore no stick"
+
