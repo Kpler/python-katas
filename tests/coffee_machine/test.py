812d5db1a1a6c39d7d123711bb080fd2d2128527
Vladislav Sokolov <vsokolov@kpler.com>
2023-12-14 14:22:33 +0100

    vlad

---
+++ tests/coffee_machine/test.py
@@ -0,0 +1,9 @@
+from src.coffee_machine.coffee_machine import CoffeeMachine
+
+
+def test_coffee_machine_can_instantiate_itself():
+    coffe_machine = CoffeeMachine()
+
+
+def test_should_return_an_empty_string_when_no_order_passed:
+    pass
