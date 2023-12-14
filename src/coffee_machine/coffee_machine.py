374a6b509649350313867af116a32322c2c61139
Daniel Halmschlager <dhalmschlager@kpler.com>
2023-12-14 15:28:52 +0100

    fix: handle stick

--- src/coffee_machine/coffee_machine.py
+++ src/coffee_machine/coffee_machine.py
@@ -23,7 +23,25 @@ class CoffeeMachine:
     sugar_quantity: int | None = None
 
     def get_order(self) -> str:
-        return f"{BEVERAGE_MAP.get(self.order_beverage, EMPTY_ORDER)}::"
+        if not self.order_beverage:
+            return EMPTY_ORDER
+        return f"{self._translate_order_beverage()}:{self._translate_order_sugar_quantity()}:{self._translate_order_stick()}"
+
+    def _translate_order_beverage(self):
+        return BEVERAGE_MAP[self.order_beverage]
+    
+    def _translate_order_sugar_quantity(self):
+        if self._is_there_sugar():
+            return self.sugar_quantity
+        return ''
+    
+    def _translate_order_stick(self):
+        if self._is_there_sugar():
+            return '0'
+        return ''
+    
+    def _is_there_sugar(self) -> bool:
+        return self.sugar_quantity > 0
 
     def send_order(self, beverage: str, sugar_quantity: int = 0) -> None:
         beverage = Beverage(beverage)
