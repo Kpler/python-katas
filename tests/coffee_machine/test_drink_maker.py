08f8e3f87f606941cf0803346b4195bac6ee291b
Georgios Skaros <gskaros@kpler.com>
2023-12-21 15:29:19 +0200

    test(sugar): add test for sugar

--- tests/coffee_machine/test_drink_maker.py
+++ tests/coffee_machine/test_drink_maker.py
@@ -19,3 +19,21 @@ def test_make_tea(
     drink_maker = DrinkMaker()
     command = drink_maker.make(order=input)
     assert command == output
+
+
+@pytest.mark.parametrize(
+    "input,output",
+    [
+        [Beverage.TEA, "T::"],
+        [Beverage.HOT_CHOCOLATE, "H::"],
+        [Beverage.COFFEE, "C::"],
+        ["Hello World", "M:Hello World"],
+    ],
+)
+def test_sugar(
+    input,
+    output,
+):
+    drink_maker = DrinkMaker()
+    command = drink_maker.make(order=input, sugar=0)
+    assert command == output 
