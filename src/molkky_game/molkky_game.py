118f78978b5682274ef990d8a65620e2878b35b7
Gary <me@garyfibiger.cz>
2024-02-01 15:09:50 +0100

    feat(calculator): add won condition

--- src/molkky_game/molkky_game.py
+++ src/molkky_game/molkky_game.py
@@ -5,6 +5,7 @@ from enum import Enum
 MAX_SCORE = 50
 CHECKPOINT_SCORE = 25
 
+
 class Pin(Enum):
     ONE = 1
     TWO = 2
@@ -24,7 +25,7 @@ class Pin(Enum):
 class MolkkyScore:
     value: int
 
-    def append(self, score: "MolkkyScore") -> "MolkkyScore":
+    def add_score(self, score: "MolkkyScore") -> "MolkkyScore":
         new_value = self.value + score.value
         if new_value > MAX_SCORE:
             new_value = CHECKPOINT_SCORE
@@ -33,22 +34,33 @@ class MolkkyScore:
 
 @dataclass(frozen=True)
 class PinScore:
-    value: list[Pin]
+    value: frozenset[Pin]
 
     def get_score(self) -> MolkkyScore:
         if len(self.value) == 1:
-            score = self.value[0].value
+            the_pin = next(iter(self.value))
+            score = the_pin.value
         else:
             score = len(self.value)
 
         return MolkkyScore(score)
 
 
+class MolkkyGameState(Enum):
+    WON = "won"
+    PLAYING = "playing"
+
+
 @dataclass(frozen=True, kw_only=True)
 class MolkkyGame:
     score: MolkkyScore = MolkkyScore(0)
+    state: MolkkyGameState = MolkkyGameState.PLAYING
 
-    def pin_down(self, pin_numbers: list[Pin]) -> "MolkkyGame":
+    def pin_down(self, pin_numbers: frozenset[Pin]) -> "MolkkyGame":
         score = PinScore(pin_numbers).get_score()
+        new_score = self.score.add_score(score)
 
-        return MolkkyGame(score=self.score.append(score))
+        if new_score.value == MAX_SCORE:
+            return MolkkyGame(score=new_score, state=MolkkyGameState.WON)
+        else:
+            return MolkkyGame(score=new_score, state=self.state)
--- tests/molkky_game/molkky_game.py
+++ tests/molkky_game/molkky_game.py
@@ -1,4 +1,4 @@
-from src.molkky_game.molkky_game import MolkkyGame, MolkkyScore, Pin
+from src.molkky_game.molkky_game import MolkkyGame, MolkkyGameState, MolkkyScore, Pin, MAX_SCORE
 
 
 def test_molkky_game():
@@ -9,29 +9,46 @@ def test_molkky_game():
 
 def test_correct_score_after_one_pin_down():
     game = MolkkyGame()
-    game = game.pin_down([Pin.ONE])
+    game = game.pin_down(frozenset({Pin.ONE}))
     assert game.score == MolkkyScore(1)
 
-    game = game.pin_down([Pin.TWO])
+    game = game.pin_down(frozenset({Pin.TWO}))
     assert game.score == MolkkyScore(3)
 
 
 def test_correct_score_when_no_pins_down():
     game = MolkkyGame()
-    game = game.pin_down([])
+    game = game.pin_down(frozenset())
 
     assert game.score == MolkkyScore(0)
 
 
 def test_correct_score_when_two_pins_down():
     game = MolkkyGame()
-    game = game.pin_down([Pin.FOUR, Pin.FIVE])
+    game = game.pin_down(frozenset({Pin.FOUR, Pin.FIVE}))
 
     assert game.score == MolkkyScore(2)
+
 
 def test_score_fifty():
     game = MolkkyGame()
     for i in range(5):
-        game = game.pin_down([Pin.ELEVEN])
-        
+        game = game.pin_down(frozenset({Pin.ELEVEN}))
+
     assert game.score == MolkkyScore(25)
+
+
+def test_should_count_pin_just_once():
+    game = MolkkyGame()
+    game = game.pin_down(frozenset({Pin.FOUR, Pin.FOUR}))
+
+    assert game.score == MolkkyScore(4)
+
+
+def test_should_be_won_on_max_score():
+    game = MolkkyGame()
+    for i in range(5):
+        game = game.pin_down(frozenset({Pin.TEN}))
+
+    assert game.state == MolkkyGameState.WON
+    assert game.score == MolkkyScore(MAX_SCORE)
