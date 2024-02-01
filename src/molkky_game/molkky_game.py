4ff3812efb761faf5f44d8564096f1e9c48dd3b2
Dennis Marten <marten@fleetmon.com>
2024-02-01 14:41:10 +0100

    feat: wip

--- src/molkky_game/molkky_game.py
+++ src/molkky_game/molkky_game.py
@@ -1,4 +1,20 @@
 from dataclasses import dataclass
+from enum import Enum
+
+
+class Pin(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    ELEVEN = 11
+    TWELVE = 12
 
 
 @dataclass(frozen=True)
@@ -6,14 +22,24 @@ class MolkkyScore:
     value: int
 
 
+@dataclass(frozen=True)
+class PinScore:
+    value: list[Pin]
+
+    def get_score(self) -> MolkkyScore:
+        if len(self.value) == 1:
+            score = self.value[0].value
+        else:
+            score = len(self.value)
+
+        return MolkkyScore(score)
+
+
 @dataclass(frozen=True, kw_only=True)
 class MolkkyGame:
     score: MolkkyScore = MolkkyScore(0)
 
-    def pin_down(self, pin_numbers: list[int]) -> "MolkkyGame":
-        if len(pin_numbers) == 1:
-            score = pin_numbers[0]
-        else:
-            score = len(pin_numbers)
+    def pin_down(self, pin_numbers: list[Pin]) -> "MolkkyGame":
+        score = PinScore(pin_numbers).get_score()
 
-        return MolkkyGame(score=MolkkyScore(self.score.value + score))
+        return MolkkyGame(score=MolkkyScore(self.score.value + score.value))
--- tests/molkky_game/molkky_game.py
+++ tests/molkky_game/molkky_game.py
@@ -1,4 +1,4 @@
-from src.molkky_game.molkky_game import MolkkyGame, MolkkyScore
+from src.molkky_game.molkky_game import MolkkyGame, MolkkyScore, Pin
 
 
 def test_molkky_game():
@@ -9,10 +9,10 @@ def test_molkky_game():
 
 def test_correct_score_after_one_pin_down():
     game = MolkkyGame()
-    game = game.pin_down([1])
+    game = game.pin_down([Pin.ONE])
     assert game.score == MolkkyScore(1)
 
-    game = game.pin_down([2])
+    game = game.pin_down([Pin.TWO])
     assert game.score == MolkkyScore(3)
 
 
@@ -25,6 +25,6 @@ def test_correct_score_when_no_pins_down():
 
 def test_correct_score_when_two_pins_down():
     game = MolkkyGame()
-    game = game.pin_down([4, 5])
+    game = game.pin_down([Pin.FOUR, Pin.FIVE])
 
     assert game.score == MolkkyScore(2)
