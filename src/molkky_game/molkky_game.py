d2a4f98f7bf5aab30964416e408f1ef2965b20cc
Vladislav Sokolov <vsokolov@kpler.com>
2024-02-01 14:21:30 +0100

    a

--- src/molkky_game/molkky_game.py
+++ src/molkky_game/molkky_game.py
@@ -10,5 +10,10 @@ class MolkkyScore:
 class MolkkyGame:
     score: MolkkyScore = MolkkyScore(0)
 
-    def pin_down(self, pin_number: int) -> "MolkkyGame":
-        return MolkkyGame(score=MolkkyScore(self.score.value + pin_number))
+    def pin_down(self, pin_numbers: list[int]) -> "MolkkyGame":
+        if len(pin_numbers) == 1:
+            score = pin_numbers[0]
+        else:
+            score = len(pin_numbers)
+
+        return MolkkyGame(score=MolkkyScore(self.score.value + score))
--- tests/molkky_game/molkky_game.py
+++ tests/molkky_game/molkky_game.py
@@ -6,8 +6,25 @@ def test_molkky_game():
     zero_score = MolkkyScore(0)
     assert game.score == zero_score
 
-    game = game.pin_down(1)
+
+def test_correct_score_after_one_pin_down():
+    game = MolkkyGame()
+    game = game.pin_down([1])
     assert game.score == MolkkyScore(1)
 
-    game = game.pin_down(2)
+    game = game.pin_down([2])
     assert game.score == MolkkyScore(3)
+
+
+def test_correct_score_when_no_pins_down():
+    game = MolkkyGame()
+    game = game.pin_down([])
+
+    assert game.score == MolkkyScore(0)
+
+
+def test_correct_score_when_two_pins_down():
+    game = MolkkyGame()
+    game = game.pin_down([4, 5])
+
+    assert game.score == MolkkyScore(2)
