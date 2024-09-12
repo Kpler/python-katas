bd0adcf09dd14fddd1bb86d558456b88e3d972bc
Vladislav Sokolov <vsokolov@kpler.com>
2024-09-12 11:38:08 +0200

    commit 4

--- src/supermarket_receipt/src/teller.py
+++ src/supermarket_receipt/src/teller.py
@@ -1,19 +1,23 @@
-from src.model_objects import Offer
+from src.catalog import SupermarketCatalog
+from src.model_objects import Offer, Product, SpecialOfferType
 from src.receipt import Receipt
+from src.shopping_cart import ShoppingCart
+
+OfferMap = dict[Product, Offer]
 
 
 class Teller:
     """The entity scanning the item.
     """
 
-    def __init__(self, catalog):
-        self.catalog = catalog
-        self.offers = {}
+    def __init__(self, catalog: SupermarketCatalog) -> None:
+        self.catalog: SupermarketCatalog = catalog
+        self.offers: OfferMap = {}
 
-    def add_special_offer(self, offer_type, product, argument):
+    def add_special_offer(self, offer_type: SpecialOfferType, product: Product, argument: float):
         self.offers[product] = Offer(offer_type, product, argument)
 
-    def checks_out_articles_from(self, the_cart):
+    def checks_out_articles_from(self, the_cart: ShoppingCart) -> Receipt:
         receipt = Receipt()
         product_quantities = the_cart.items
         for pq in product_quantities:
@@ -26,6 +30,3 @@ class Teller:
         the_cart.handle_offers(receipt, self.offers, self.catalog)
 
         return receipt
-
-    def product_with_name(self, name):
-        return self.catalog.products.get(name, None)
