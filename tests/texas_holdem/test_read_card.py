a30c317ebb5fb8e292ab73250fc7a22a2911f87a
rekahrv <rhorvath@kpler.com>
2025-02-19 16:14:17 +0100

    test: failing test

--- tests/texas_holdem/test_read_card.py
+++ tests/texas_holdem/test_read_card.py
@@ -4,3 +4,7 @@ from src.texas_holdem.card import Card, Suits, Ranks
 
 def test_read_1_card():
     assert read_card("Kc") == Card(suit=Suits.CLUBS, rank=Ranks.KING)
+
+
+def test_read_1_card_other_example():
+    assert read_card("Ah") == Card(suit=Suits.HEARTS, rank=Ranks.ACE)
