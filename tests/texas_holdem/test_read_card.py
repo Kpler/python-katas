86331f262b19d816bd4bccd91021e240c175b99f
rekahrv <rhorvath@kpler.com>
2025-02-19 16:49:07 +0100

    refactor: create test_read_hand

--- tests/texas_holdem/test_read_card.py
+++ tests/texas_holdem/test_read_card.py
@@ -17,16 +17,3 @@ testdata = [
 @pytest.mark.parametrize("expected, input", testdata)
 def test_read_1_card(expected, input):
     assert read_card(input) == expected
-
-
-# Kc 9s Ks Kd 9d 3c 6d
-def test_read_multi_card_hand():
-    assert read_hand("Kc 9s Ks Kd 9d 3c 6d") == [
-        Card(rank=Ranks.KING, suit=Suits.CLUBS),
-        Card(rank=Ranks.NINE, suit=Suits.SPADES),
-        Card(rank=Ranks.KING, suit=Suits.SPADES),
-        Card(rank=Ranks.KING, suit=Suits.DIAMONDS),
-        Card(rank=Ranks.NINE, suit=Suits.DIAMONDS),
-        Card(rank=Ranks.THREE, suit=Suits.CLUBS),
-        Card(rank=Ranks.SIX, suit=Suits.DIAMONDS),
-    ]
---
+++ tests/texas_holdem/test_read_hand.py
@@ -0,0 +1,14 @@
+from src.texas_holdem.card import Card, Ranks, Suits
+from src.texas_holdem.input import read_hand
+
+
+def test_read_multi_card_hand():
+    assert read_hand("Kc 9s Ks Kd 9d 3c 6d") == [
+        Card(rank=Ranks.KING, suit=Suits.CLUBS),
+        Card(rank=Ranks.NINE, suit=Suits.SPADES),
+        Card(rank=Ranks.KING, suit=Suits.SPADES),
+        Card(rank=Ranks.KING, suit=Suits.DIAMONDS),
+        Card(rank=Ranks.NINE, suit=Suits.DIAMONDS),
+        Card(rank=Ranks.THREE, suit=Suits.CLUBS),
+        Card(rank=Ranks.SIX, suit=Suits.DIAMONDS),
+    ]
