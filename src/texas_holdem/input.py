2718d2d60a9724f804fa1b06f51e69600df28012
Fabien Bousquet <fbousquet@kpler.com>
2025-02-19 16:29:14 +0100

    chore : add card reader

--- src/texas_holdem/input.py
+++ src/texas_holdem/input.py
@@ -1,5 +1,28 @@
 from src.texas_holdem.card import Card, Suits, Ranks
 
 
+rank_symbol = {
+    "2": Ranks.TWO,
+    "3": Ranks.THREE,
+    "4":Ranks.FOUR,
+    "5": Ranks.FIVE,
+    "6":Ranks.SIX,
+    '7': Ranks.SEVEN,
+    '8': Ranks.EIGHT,
+    '9': Ranks.NINE,
+    'T': Ranks.TEN,
+    'J': Ranks.JACK,
+    'Q': Ranks.QUEEN,
+    'K': Ranks.KING,
+    'A': Ranks.ACE }
+
+suit_symbol =  {
+    'c': Suits.CLUBS,
+    'd': Suits.DIAMONDS,
+    'h': Suits.HEARTS,
+    's': Suits.SPADES,
+}
+
+
 def read_card(text: str) -> Card:
-    return Card(suit=Suits.CLUBS, rank=Ranks.KING)
+    return Card(rank=rank_symbol[text[0]],suit=suit_symbol[text[1]] )
