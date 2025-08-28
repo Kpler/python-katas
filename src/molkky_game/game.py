f4fe84309d80d72621916f7148525c20efd66757
vgarcin <vperiegarcin@kpler.com>
2025-08-28 16:36:46 +0200

    add kick on 1 pin

--- src/molkky_game/game.py
+++ src/molkky_game/game.py
@@ -3,3 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class MolkkyGame:
     score: int = 0
+    numberPinsDown: int = 0
+
+    def kickPins(self, param):
+        self.numberPinsDown += len(param)
+        self.score += param[0]
--- tests/molkky_game/test_init_game.py
+++ tests/molkky_game/test_init_game.py
@@ -3,3 +3,9 @@ from molkky_game.game import MolkkyGame
 def test_init_game():
     game = MolkkyGame()
     assert game.score == 0
+
+def test_score_one_pin():
+    game = MolkkyGame()
+    game.kickPins([5])
+    assert game.numberPinsDown == 1
+    assert game.score == 5
