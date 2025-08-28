34191024ff9e894474f579cde7484bf3067e64a4
rekahrv <rhorvath@kpler.com>
2025-08-28 16:50:01 +0200

    refactor: renaming

--- src/molkky_game/game.py
+++ src/molkky_game/game.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+from typing import List
+
 
 @dataclass
 class MolkkyGame:
-    numberPinsDown: int[] = []
+    number_pins_down: List[int] = []
     score: int = 0
 
-    def kickPins(self, param):
-        self.numberPinsDown += len(param)
+    def kick_pins(self, param):
+        self.number_pins_down += len(param)
         self.score += param[0]
--- tests/molkky_game/test_init_game.py
+++ tests/molkky_game/test_init_game.py
@@ -1,17 +1,20 @@
 from molkky_game.game import MolkkyGame
 
+
 def test_init_game():
     game = MolkkyGame()
     assert game.score == 0
 
+
 def test_score_one_pin():
     game = MolkkyGame()
-    game.kickPins([5])
-    assert game.numberPinsDown == 1
+    game.kick_pins([5])
+    assert game.number_pins_down == 1
     assert game.score == 5
+
 
 def test_score_several_pins():
     game = MolkkyGame()
-    game.kickPins([5,3,1])
-    assert game.numberPinsDown == [5,3,1]
+    game.kick_pins([5, 3, 1])
+    assert game.number_pins_down == [5, 3, 1]
     assert game.score == 3
