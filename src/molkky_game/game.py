14a166ea15d1204e5534130183b3d9260f719e0a
rekahrv <rhorvath@kpler.com>
2025-08-28 16:58:20 +0200

    feat: update score

--- src/molkky_game/game.py
+++ src/molkky_game/game.py
@@ -4,9 +4,12 @@ from typing import List
 
 @dataclass
 class MolkkyGame:
-    number_pins_down: List[int] = []
+    pins_down: List[int]
     score: int = 0
 
     def kick_pins(self, param):
-        self.number_pins_down += len(param)
-        self.score += param[0]
+        self.pins_down = param
+        if len(param) == 1:
+            self.score += param[0]
+        else:
+            self.score += len(param)
--- tests/molkky_game/test_init_game.py
+++ tests/molkky_game/test_init_game.py
@@ -2,19 +2,19 @@ from molkky_game.game import MolkkyGame
 
 
 def test_init_game():
-    game = MolkkyGame()
+    game = MolkkyGame(pins_down=[])
     assert game.score == 0
 
 
 def test_score_one_pin():
-    game = MolkkyGame()
+    game = MolkkyGame(pins_down=[])
     game.kick_pins([5])
-    assert game.number_pins_down == 1
+    assert game.pins_down == [5]
     assert game.score == 5
 
 
 def test_score_several_pins():
-    game = MolkkyGame()
+    game = MolkkyGame(pins_down=[])
     game.kick_pins([5, 3, 1])
-    assert game.number_pins_down == [5, 3, 1]
+    assert game.pins_down == [5, 3, 1]
     assert game.score == 3
