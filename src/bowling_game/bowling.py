22611a66f66978127526d0002c9ddc7b3a15d270
Bousquet <fbousquet@kpler.com>
2025-10-09 16:47:02 +0200

    strike use case

--- src/bowling_game/bowling.py
+++ src/bowling_game/bowling.py
@@ -2,9 +2,10 @@ class Frame:
     def __init__(self):
         self.first_roll = None
         self.second_roll = None
+        self.bonuses = []
 
     def score(self):
-        return self.first_roll + self.second_roll
+        return self.first_roll + self.second_roll + sum(self.bonuses)
 
     def add_roll(self, score):
         if self.first_roll is None:
@@ -19,15 +20,19 @@ class Game:
         self.current_score = 0
         self.frames = [Frame()]
 
-    # TODO - add the consept of the bonus
+
     def roll(self, score: int):
         first_roll = self.frames[-1][1] is None
-        if first_roll:
-            self.frames[-1].add_roll(score)
+        self.frames[-1].add_roll(score)
+        if not first_roll:
+            self.frames.append(Frame())
+        if self.frames[-2].first_roll == 10:
+            self.frames[-2].bonuses.append(score)
+
         # self.current_score += score
 
         # self.frame = self.frame + 1 if not self.first_roll else self.frame
         pass
-
+# TODO : handle strike
     def score(self):
         return self.current_score
