a1b9c547cc2e7e4d96f0b3f837fe1ff545e93bd4
saad <sbalbiyad@kpler.com>
2025-10-29 17:15:43 +0100

    add test cases

--- src/bowling_game/bowling.py
+++ src/bowling_game/bowling.py
@@ -1,16 +1,28 @@
 
 
 class Game:
-
-
     def __init__(self):
-        self.rolls = [0]*21
-        self.current_roll = 0
-        self.score = 0
+        self.rolls: list[int] = [0] * 21
+        self.current_roll: int = 0
+        self.score: int = 0
+        self.is_last_frame_a_strike: bool = False
 
-    def roll(self,pins:int):
+    def roll(self, pins: int):
+        print(self.get_game_state())
         self.rolls[self.current_roll] = pins
         self.current_roll += 1
+        self.update_score(pins)
+        if (pins == 10 and self.current_roll%2 == 1):
+            self.is_last_frame_a_strike = True
+            self.current_roll += 1
+        elif self.current_roll%2 == 0:
+            self.is_last_frame_a_strike = False
 
-    def score(self, score:int):
-        self.score += score
+
+    def update_score(self, pins: int):
+        self.score += pins
+        if self.is_last_frame_a_strike:
+            self.score += pins
+    
+    def get_game_state(self):
+        return (self.current_roll, self.score, self.is_last_frame_a_strike)
--- tests/bowling_game/test_bowling.py
+++ tests/bowling_game/test_bowling.py
@@ -15,3 +15,64 @@ def test_bad_player() ->  None:
     assert(g.current_roll == 1)
     assert(g.score == 0)
     assert(g.rolls == [0]*21)
+
+def test_one_roll() ->  None:
+    g = Game()
+    g.roll(5)
+    assert(g.current_roll == 1)
+    assert(g.score == 5)
+    assert(g.rolls[0] == 5)
+    assert(g.rolls[1:] == [0]*20)
+
+def test_bad_score() ->  None:
+    g = Game()
+    bad_roll = 3
+    for _ in range(20):
+        g.roll(bad_roll)
+    assert(g.current_roll == 20)
+    assert(g.score == 60)
+    assert(g.rolls[:-1] == [3]*20)
+    assert(g.rolls[-1] == 0)
+
+def test_perfect_score() ->  None:
+    g = Game()
+    perfect_roll = 10
+    for _ in range(10):
+        g.roll(perfect_roll)
+    assert(g.current_roll == 21)
+    assert(g.score == 300)
+
+
+def test_strike() ->  None:
+    """
+    frame1: +10
+    frame2: +5 , +5
+    """
+    g = Game()
+    perfect_roll = 10
+    normal_roll = 5
+    # first frame
+    g.roll(perfect_roll)
+    # second frame
+    g.roll(normal_roll)
+    g.roll(normal_roll)
+    assert(g.current_roll == 4)
+    assert(g.score == 30)
+
+def test_two_consecutive_strikes() ->  None:
+    """
+    frame1: +10
+    frame2: +10
+    """
+    g = Game()
+    perfect_roll = 10
+    normal_roll = 5
+    # first frame
+    g.roll(perfect_roll)
+    # second frame
+    g.roll(perfect_roll) # bonus_frame_1 = 10
+    # third frame
+    g.roll(normal_roll) # bonus_frame_1 += 5, bonus_frame_2 += 5
+    g.roll(normal_roll) # bonus_frame_2 += 5
+    assert(g.current_roll == 6)
+    assert(g.score == 55)
